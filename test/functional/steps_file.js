a73accb12f8caadc910150fce47345cd1750509c
Jakub Kozłowski <jakub.kozlowski@gmail.com>
2021-06-29 11:33:58 +0100

    Functional tests (skeleton) (#2)
    * Trying to pass sonar quality gate
    
    * Adding functional tests skeleton
    
    * Fixing lint

---
+++ test/functional/steps_file.js
@@ -0,0 +1,13 @@
+
+// in this file you can append custom step methods to 'I' object
+
+module.exports = () => {
+  return actor({
+
+    // Define custom steps here, use 'this' to access default methods of I.
+    // It is recommended to place a general 'login' function here.
+    pleaseReplaceWithFirstCustomMethod() {
+      // not empty
+    }
+  });
+};
--- test/unit/definitions/CaseField.test.js
+++ test/unit/definitions/CaseField.test.js
@@ -1,9 +1,7 @@
 const { expect } = require('chai');
 
 describe('CaseField', () => {
-
-    it('this is fake test', () => {
-      expect(1).to.eql(1);
-    });
-
+  it('this is fake test', () => {
+    expect(1).to.eql(1);
+  });
 });
