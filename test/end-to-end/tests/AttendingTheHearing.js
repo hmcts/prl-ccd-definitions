215549a760452352f0d1edc0c20fcb5d332e790f
Poojitha Nagappa <poojithanag@gmail.com>
2021-11-11 14:41:49 +0000

    Added wait on Create Case

--- test/end-to-end/tests/AttendingTheHearing.js
+++ test/end-to-end/tests/AttendingTheHearing.js
@@ -3,5 +3,6 @@ Feature('Attending the hearing');
 Scenario('Attending the hearing - basic journey', async I => {
   await I.loginAsSolicitor();
   await I.createCase();
+  await I.runPeopleInTheCaseEvent();
   await I.runAttendingTheHearingEvent();
 }).retry({ retries: 3, minTimeout: 30000 });
