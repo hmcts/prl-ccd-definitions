1626fd16f65c7020f32eab42c4ba62c0c707e486
Poojitha Nagappa <poojithanag@gmail.com>
2021-11-16 12:12:48 +0000

    PRL-446 and PR-421

--- test/end-to-end/tests/AttendingTheHearing.js
+++ test/end-to-end/tests/AttendingTheHearing.js
@@ -3,7 +3,5 @@ Feature('Attending the hearing');
 Scenario('Attending the hearing - basic journey', async I => {
   await I.loginAsSolicitor();
   await I.createCase();
-  await I.changeCaseName();
-  await I.runPeopleInTheCaseEvent();
   await I.runAttendingTheHearingEvent();
 }).retry({ retries: 3, minTimeout: 30000 });
