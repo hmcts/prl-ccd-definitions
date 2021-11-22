6f16748948877e3438b8ce0b2623b291110f92fa
Raghu Nakkala <nraghu31@gmail.com>
2021-11-22 13:35:10 +0000

    E2E for Litigation capacity

---
+++ test/end-to-end/pages/LitigationCapacity.js
@@ -0,0 +1,25 @@
+const I = actor();
+
+module.exports = {
+  fields: {
+    headerText: 'Litigation capacity',
+    textareaText: 'Testing text area',
+    litigationCapacityFactors: 'textarea[id="litigationCapacityFactors"]',
+    litigationCapacityReferrals: 'textarea[id="litigationCapacityReferrals"]',
+    litigationCapacityOtherFactors: 'input[id="litigationCapacityOtherFactors_Yes"]',
+    litigationCapacityOtherFactorsDetails: 'textarea[id="litigationCapacityOtherFactorsDetails"]'
+  },
+  async  litigationCapacity() {
+    await I.triggerEvent(this.fields.headerText);
+    await I.waitForPage('h1', this.fields.headerText);
+    await I.fillField(this.fields.litigationCapacityFactors, this.fields.textareaText);
+    await I.fillField(this.fields.litigationCapacityReferrals, this.fields.textareaText);
+    await I.click(this.fields.litigationCapacityOtherFactors);
+    await I.wait('2');
+    await I.fillField(this.fields.litigationCapacityOtherFactorsDetails, this.fields.textareaText);
+    await I.click('Continue');
+    await I.waitForText('Submit', '30');
+    await I.click('Submit');
+  }
+
+};
