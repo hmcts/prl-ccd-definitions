789271f30e173c42d127a4440d2f0cd996c3a7f7
Swaroopa Pendyala <pendyalaswaroopa86@gmail.com>
2021-11-25 17:02:25 +0000

    testcases are added

--- test/end-to-end/tests/OtherPeopleInTheCase.js
+++ test/end-to-end/tests/OtherPeopleInTheCase.js
@@ -1,7 +1,7 @@
 Feature('Other people in the case');
 
 Scenario('Other people in the case event', async I => {
-    await I.loginAsSolicitor();
-    await I.createCase();
-    await I.otherPeopleInTheCase();
+  await I.loginAsSolicitor();
+  await I.createCase();
+  await I.otherPeopleInTheCase();
 }).retry({ retries: 3, minTimeout: 30000 });
