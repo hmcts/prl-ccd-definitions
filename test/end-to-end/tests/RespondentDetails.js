1eb9f92ae0232d4d55ff3786bff1bc6eed5320a9
pete-lowe <petemlowe@gmail.com>
2021-12-02 17:14:28 +0000

    test formatting changes

--- test/end-to-end/tests/RespondentDetails.js
+++ test/end-to-end/tests/RespondentDetails.js
@@ -1,7 +1,7 @@
 Feature('Respondent details');
 
 Scenario('Respondent details event', async I => {
-    await I.loginAsSolicitor();
-    await I.createCase();
-    await I.respondentDetails();
+  await I.loginAsSolicitor();
+  await I.createCase();
+  await I.respondentDetails();
 }).retry({ retries: 3, minTimeout: 30000 });
