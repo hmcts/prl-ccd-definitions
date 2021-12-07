311ff4bb973ca08e3cb716509c36298f216a6283
yogendraU <66823759+yogendraU@users.noreply.github.com>
2021-12-07 17:15:01 +0000

    Update steps_file.js
    Removed safeguarding event from tests

--- test/end-to-end/steps_file.js
+++ test/end-to-end/steps_file.js
@@ -4,7 +4,6 @@ const PeopleInTheCasePage = require('./pages/PeopleInTheCase');
 const UploadDocuments = require('./pages/UploadDocuments');
 const TypeOfApplicationEvent = require('./pages/TypeOfApplication');
 const AttendingTheHearing = require('./pages/AttendingTheHearing');
-const safeguardAndRiskOfHarmPage = require('./pages/safeguardingAndRiskOfHarm/safeguardingAndRiskOfHarm.js');
 const Miam = require('./pages/Miam.js');
 const ChildDetails = require('./pages/ChildDetails');
 const ApplicantDetails = require('./pages/ApplicantDetails');
@@ -37,9 +36,6 @@ module.exports = () => {
     },
     uploadDocuments() {
       return UploadDocuments.uploadDocuments();
-    },
-    safeguardingAndRiskOfHarm() {
-      return safeguardAndRiskOfHarmPage.safeguardAndRiskOfHarmEvent();
     },
     typeOfApplicationEvent() {
       return TypeOfApplicationEvent.typeOfApplicationEvent();
