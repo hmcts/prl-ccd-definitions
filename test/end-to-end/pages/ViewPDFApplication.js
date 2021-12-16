a53fba7c99b58128f685a65469c8bd0a85d4edee
pete-lowe <petemlowe@gmail.com>
2021-12-16 14:50:09 +0000

    added asterisk to hearing urgency event

--- test/end-to-end/pages/ViewPDFApplication.js
+++ test/end-to-end/pages/ViewPDFApplication.js
@@ -2,23 +2,21 @@ const I = actor();
 
 module.exports = {
 
-    fields: {
-        submit: 'button[type="submit"]',
-    },
+  fields: { submit: 'button[type="submit"]' },
 
-    async triggerEvent() {
-        await I.triggerEvent('View PDF application');
-    },
+  async triggerEvent() {
+    await I.triggerEvent('View PDF application');
+  },
 
-    async downloadApplication() {
-        await I.waitForText('Download Application', '30');
-        await I.click(this.fields.submit);
-    },
+  async downloadApplication() {
+    await I.waitForText('Download Application', '30');
+    await I.click(this.fields.submit);
+  },
 
-    async runViewPDFApplicationEvent() {
-        await this.triggerEvent();
-        await this.downloadApplication();
-        await I.click('Save and continue');
-        await I.amOnHistoryPageWithSuccessNotification();
-    }
+  async runViewPDFApplicationEvent() {
+    await this.triggerEvent();
+    await this.downloadApplication();
+    await I.click('Save and continue');
+    await I.amOnHistoryPageWithSuccessNotification();
+  }
 };
