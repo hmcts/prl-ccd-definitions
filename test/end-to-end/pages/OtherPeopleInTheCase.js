40af75bc79e21f63715d7af5b645f1de62318ae6
pete-lowe <petemlowe@gmail.com>
2022-01-12 09:46:30 +0000

    updated automated tests

--- test/end-to-end/pages/OtherPeopleInTheCase.js
+++ test/end-to-end/pages/OtherPeopleInTheCase.js
@@ -29,6 +29,8 @@ module.exports = {
     await I.fillField('//input[@id="othersToNotify_0_email"]', 'otherstonotify@email.com');
     await I.retry(retryCount).checkOption('//input[@id="othersToNotify_0_canYouProvidePhoneNumber_Yes"]');
     await I.fillField('//input[@id="othersToNotify_0_phoneNumber"]', '07122884667');
+    await I.click('Add new');
+    await I.fillField('//input[@id="othersToNotify_0_otherPersonRelationshipToChildren_0_personRelationshipToChild"]', 'Father');
     await I.click('Continue');
     await I.waitForText('Save and continue', '30');
     await I.click('Save and continue');
