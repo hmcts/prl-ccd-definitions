305c806a4aea55b1e7936fa1b1d3d17988485d84
Yogendra Upasani <yogendra.upasani@hmcts.net>
2022-01-17 23:38:36 +0000

    Updated test case

--- test/end-to-end/steps_file.js
+++ test/end-to-end/steps_file.js
@@ -7,62 +7,14 @@ module.exports = () => {
     loginAsSolicitor() {
       return LoginPage.loginAsSolicitor();
     },
-    runAttendingTheHearingEvent() {
-      return AttendingTheHearing.runEventHappyPathAttendingTheHearing();
-    },
-    runPeopleInTheCaseEvent() {
-      return PeopleInTheCasePage.runEventHappyPath();
-    },
     triggerEvent(eventName) {
       return generalHelper.triggerEvent(eventName);
     },
     createCase() {
       return CreateCasePage.createNewCase();
     },
-    uploadDocuments() {
-      return UploadDocuments.uploadDocuments();
-    },
-    typeOfApplicationEvent() {
-      return TypeOfApplicationEvent.typeOfApplicationEvent();
-    },
-    runMIAMEventHappyPath() {
-      return Miam.runMIAMEventHappyPath();
-    },
     childDetails() {
       return ChildDetails.runChildDetailsEventHappyPath();
-    },
-    applicantDetails() {
-      return ApplicantDetails.runApplicantDetailsEventHappyPath();
-    },
-    caseNameChange() {
-      return CaseName.runEventCaseName();
-    },
-    hearingUrgency() {
-      return HearingUrgency.hearingUrgency();
-    },
-    litigationCapacity() {
-      return LitigationCapacity.litigationCapacity();
-    },
-    otherPeopleInTheCase() {
-      return OtherPeopleInTheCase.otherPeopleInTheCase();
-    },
-    internationalElement() {
-      return InternationalElement.internationalElement();
-    },
-    respondentDetails() {
-      return RespondentDetails.respondentDetails();
-    },
-    welshLanguageRequirement() {
-      return WelshLanguage.welshLanguageRequirement();
-    },
-    otherProceedingsEvent() {
-      return otherProceedings.otherProceedingsEvent();
-    },
-    allegationsOfHarmEvent() {
-      return allegationsOfHarm.allegationsOfHarmEvent();
-    },
-    viewPDFApplicationEvent() {
-      return viewPDFApplication.runViewPDFApplicationEvent();
     }
   });
 };
