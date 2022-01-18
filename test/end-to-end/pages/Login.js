1cf874ec317bf726c210ae23a2e6f5e6bdeef3e6
Poojitha Nagappa <poojithanag@gmail.com>
2022-01-18 13:20:06 +0000

    LoginPageFixed

--- test/end-to-end/pages/Login.js
+++ test/end-to-end/pages/Login.js
@@ -7,15 +7,16 @@ const baseUrl = config.baseUrl;
 module.exports = {
 
   fields: {
-    email: 'username',
-    password: 'password',
+    email: '#username',
+    password: '#password',
     submit: 'input[type="submit"]'
   },
 
   async loginAsSolicitor() {
     await I.amOnPage(`${process.env.XUI_WEB_URL}`);
     await I.click('#cookie-accept-submit');
-    await I.seeElement('#authorizeCommand > div.form-section > div.login-list > input.button');
+    await I.click('#cookie-accept-all-success-banner-hide');
+    await I.seeElement('#authorizeCommand');
     await I.fillField(this.fields.email, config.legalProfessionalUserOne.email);
     await I.fillField(this.fields.password, config.legalProfessionalUserOne.password);
     await I.click(this.fields.submit);
