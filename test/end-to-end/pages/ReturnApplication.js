a864d17b93ad432cea34bac891f0541b540bdc1b
pete-lowe <petemlowe@gmail.com>
2022-01-25 13:11:28 +0000

    updated end-to-end

--- test/end-to-end/pages/ReturnApplication.js
+++ test/end-to-end/pages/ReturnApplication.js
@@ -2,13 +2,13 @@ const I = actor();
 
 module.exports = {
   fields: {
-   consentOrderNotProvided: 'input[id="rejectReason-consentOrderNotProvided"]',
-   miamCertificateNotProvided: 'input[id="rejectReason-miamCertificateNotProvided"]',
-   incompleteEvidenceOfMiamExamption: 'input[id="rejectReason-incompleteEvidenceOfMiamExamption"]',
-   confidentalDetailListed: 'input[id="rejectReason-confidentalDetailListed"]',
-   clarificationNeeded: 'input[id="rejectReason-clarificationNeeded"]',
-   otherReason: 'input[id="rejectReason-otherReason"]',
-   returnMessage: 'input[id="returnMessage"]'
+    consentOrderNotProvided: 'input[id="rejectReason-consentOrderNotProvided"]',
+    miamCertificateNotProvided: 'input[id="rejectReason-miamCertificateNotProvided"]',
+    incompleteEvidenceOfMiamExamption: 'input[id="rejectReason-incompleteEvidenceOfMiamExamption"]',
+    confidentalDetailListed: 'input[id="rejectReason-confidentalDetailListed"]',
+    clarificationNeeded: 'input[id="rejectReason-clarificationNeeded"]',
+    otherReason: 'input[id="rejectReason-otherReason"]',
+    returnMessage: 'input[id="returnMessage"]'
   },
 
   async triggerEvent() {
@@ -25,24 +25,14 @@ module.exports = {
     await I.click(this.fields.clarificationNeeded);
     await I.click(this.fields.otherReason);
     await I.click('Continue');
+    await I.wait('2');
 
     await I.see('Return message');
+    await I.seeElement('<textarea class="form-control bottom-30 ng-touched ng-pristine ng-valid" rows="3" id="returnMessage"></textarea>');
 
-    const text = await page.evaluate(() => {
-            const rm = document.getElementById(this.fields.returnMessage);
-            return rm.textContent;
-        })
+    await I.click('Continue');
+    await I.wait('2');
 
-    const fs = require("fs");
-
-    fs.readFile('test/resource' + "/returnApplicationContent.txt", (error, data) => {
-        if(error) {
-            throw error;
-        }
-        const dataString = data.toString();
-    });
-
-    await expect(text).toBe(dataString)
 
     await I.waitForText('Save and continue', '30');
     await I.click('Save and continue');
--- test/end-to-end/tests/ReturnApplication.js
+++ test/end-to-end/tests/ReturnApplication.js
@@ -3,5 +3,5 @@ Feature('Return application');
 Scenario('Create an event and submit Return application event', async I => {
   await I.loginAsCourtAdmin();
   await I.createCase();
-  //await I.runPeopleInTheCaseEvent();
+  // await I.runPeopleInTheCaseEvent();
 }).retry({ retries: 3, minTimeout: 30000 });
