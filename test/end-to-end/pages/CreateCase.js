ad4a0cdc3a80f97dbf5798ab80892726bb9104dc
saravanakumar-palanichamy <95418983+saravanakumar-palanichamy@users.noreply.github.com>
2022-01-26 12:11:11 +0000

    Update CreateCase.js

--- test/end-to-end/pages/CreateCase.js
+++ test/end-to-end/pages/CreateCase.js
@@ -26,22 +26,49 @@ module.exports = {
     await I.waitForClickable(this.fields.submit);
     await I.click(this.fields.submit);
   },
-  async fillCaseTypeOfApplication() {
-      const retryCount = 3;
-      await I.retry(retryCount).checkOption('//input[@id="caseTypeOfApplication-C100"]');
-      await I.click('Continue');
+
+  async selectTypeOfApplicationC100() {
+    await I.waitForText('Type of application');
+    await I.click('#caseTypeOfApplication-C100');
+    await I.click('Continue');
   },
-  async fillSolicitorApplicationPage() {
+
+  async selectTypeOfApplicationFL401() {
+    await I.waitForText('Type of application');
+    await I.click('#caseTypeOfApplication-FL401');
+    await I.click('Continue');
+  },
+
+  async fillSolicitorApplicationPageC100() {
     await I.waitForElement('#applicantCaseName');
     await I.fillField('//input[@id="applicantCaseName"]', 'Test Child');
     await I.click('Continue');
   },
 
-  async createNewCase() {
+  async fillSolicitorApplicationPageFL401() {
+    await I.waitForText('Confidentiality Statement');
+    await I.click('#confidentialityStatementDisclaimer-confidentialityStatementUnderstood');
+    await I.click('Continue');
+
+    await I.waitForElement('#applicantOrRespondentCaseName');
+    await I.fillField('#applicantOrRespondentCaseName', 'Applicant & Respondent');
+    await I.click('Continue');
+  },
+
+  async createNewCaseC100() {
     await this.clickCreateCase();
     await this.fillFormAndSubmit();
-    await this.fillCaseTypeOfApplication();
-    await this.fillSolicitorApplicationPage();
+    await this.selectTypeOfApplicationC100();
+    await this.fillSolicitorApplicationPageC100();
+    await I.submitEvent();
+    await I.amOnHistoryPageWithSuccessNotification();
+  },
+
+  async createNewCaseFL401() {
+    await this.clickCreateCase();
+    await this.fillFormAndSubmit();
+    await this.selectTypeOfApplicationFL401();
+    await this.fillSolicitorApplicationPageFL401();
     await I.submitEvent();
     await I.amOnHistoryPageWithSuccessNotification();
   }
