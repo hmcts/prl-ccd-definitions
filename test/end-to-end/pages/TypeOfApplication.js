6421f38bafc09880d5ef72d052005b01656b26dc
Jahnavi <jahnavi1satya@gmail.com>
2022-02-10 12:07:55 +0000

    Add typeOfApplicationEventFL401 to the same page

--- test/end-to-end/pages/TypeOfApplication.js
+++ test/end-to-end/pages/TypeOfApplication.js
@@ -10,7 +10,7 @@ module.exports = {
     childArrangementsOrder: 'input[id="ordersApplyingFor-childArrangementsOrder"]',
     prohibitedStepsOrder: 'input[id="ordersApplyingFor-prohibitedStepsOrder"]',
     specificIssueOrder: 'input[id="ordersApplyingFor-specificIssueOrder"]',
-    textareaText: 'Testing text area',
+    textAreaText: 'Testing text area',
     natureOfOrderTextArea: 'textarea[id="natureOfOrder"]',
     permissionRequiredRadioButton: 'input[id="applicationPermissionRequired-yes"]',
     appPermissionRequiredReason: 'textarea[id="applicationPermissionRequiredReason"]',
@@ -28,7 +28,19 @@ module.exports = {
     appWithoutNoticeRespondentWllFrustrate: 'input[id="isHearingWithoutNoticeRequiredRespondentWillFrustrate_Yes"]',
     appWithoutNoticeRespondentWllFrustrateTextArea: 'textarea[id="applicationWithoutNoticeRespondentWillFrustrateReasons"]',
     consentOrderYes: '#consentOrder_Yes',
-    typeOfChildArrangementsOrder: '#typeOfChildArrangementsOrder-bothLiveWithAndSpendTimeWithOrder'
+    typeOfChildArrangementsOrder: '#typeOfChildArrangementsOrder-bothLiveWithAndSpendTimeWithOrder',
+    nonMolestationOrder: '#typeOfApplicationOrders_orderType-nonMolestationOrder',
+    occupationOrder: '#typeOfApplicationOrders_orderType-occupationOrder',
+    ordersApplyingForPageHeader: 'Which order(s) are you applying for?',
+    ordersApplyingForPageMandatoryText: 'Which order(s) are you applying for? is required',
+    linkToChildArrangementsApplicationHeader: 'Is this linked to Child Arrangements application? (Optional)',
+    linkedChildArrangementsApplicationYes: '#typeOfApplicationLinkToCA_linkToCaApplication_Yes',
+    childArrangementOrderInstructionText: 'If you have also completed a Child Arrangements Order application enter the case number below.',
+    childArrangementCaseNumberLabel: 'Child Arrangements Case Number (FamilyMan cases not supported) (Optional)',
+    childArrangementsCaseNumberField: '#typeOfApplicationLinkToCA_caApplicationNumber',
+    //Case Number Regex validation error message to be introduced
+    childArrangementsCaseNumberText: '1234123412341234',
+    checkYourAnswersPageHeader: 'Check your answers'
   },
 
   async actionTypeOfApplicationEvent() {
@@ -38,7 +50,7 @@ module.exports = {
     await I.click(this.fields.submit);
   },
 
-  async whatOrdersPage() {
+  async whatOrdersPageC100() {
     await I.waitForText('What order(s) are you applying for?');
     await I.click(this.fields.childArrangementsOrder);
     await I.click(this.fields.prohibitedStepsOrder);
@@ -50,7 +62,7 @@ module.exports = {
     await I.click(this.fields.submit);
   },
 
-  async draftConsentOrder() {
+  async draftConsentOrderC100() {
     await I.waitForText('Do you have a draft consent order?');
     await I.click(this.fields.consentOrderYes);
     await I.attachDocument('draftConsentOrderFile');
@@ -58,34 +70,66 @@ module.exports = {
     await I.click('Continue');
   },
 
-  async permissionsPage() {
+  async permissionsPageC100() {
     await I.waitForText('Have you applied to the court for permission to make this application?');
     await I.click(this.fields.permissionRequiredRadioButton);
     await I.fillField(this.fields.appPermissionRequiredReason, this.fields.textareaText);
     await I.click(this.fields.submit);
   },
 
-  async briefDetailsPage() {
+  async briefDetailsPageC100() {
     await I.wait('2');
     await I.waitForText('Provide brief details of:');
     await I.fillField(this.fields.appDetailsTextArea, this.fields.textareaText);
     await I.click(this.fields.submit);
   },
 
-  async checkYourAnswersPage() {
-    await I.waitForText('Check your answers');
+  async checkYourAnswersPageC100() {
+    await I.waitForText(this.fields.checkYourAnswersPageHeader);
     await I.waitForText(this.fields.textareaText);
     await I.seeDocuments('Draft Consent Order', 'dummy.pdf');
     await I.click('Save and continue');
   },
 
+  async ordersApplyingForPageFL401() {
+    await I.waitForText(this.fields.ordersApplyingForPageHeader);
+    //Checking mandatory field validation
+    await I.click(this.fields.submit);
+    await I.waitForText(this.fields.ordersApplyingForPageMandatoryText);
+    await I.click(this.fields.nonMolestationOrder);
+    await I.click(this.fields.occupationOrder);
+    await I.click('Continue');
+  },
+  
+  async linkToChildArrangementsApplicationFL401() {
+    await I.waitForText(this.fields.linkToChildArrangementsApplicationHeader);
+    await I.click(this.fields.linkedChildArrangementsApplicationYes);
+    //await I.wait('5');
+    await I.waitForText(this.fields.childArrangementOrderInstructionText);
+    await I.waitForText(this.fields.childArrangementCaseNumberLabel);
+    await I.fillField(this.fields.childArrangementsCaseNumberField, this.fields.childArrangementsCaseNumberText);   
+    await I.click('Continue');
+  },
+  
+  async checkYourAnswersPageFL401() {
+    await I.waitForText(this.fields.checkYourAnswersPageHeader);
+    await I.waitForText(this.fields.childArrangementsCaseNumberText);
+    await I.click('Save and continue');
+  },
+
   async typeOfApplicationEvent() {
     await this.actionTypeOfApplicationEvent();
-    await this.whatOrdersPage();
-    await this.draftConsentOrder();
-    await this.permissionsPage();
-    await this.briefDetailsPage();
-    await this.checkYourAnswersPage();
+    await this.whatOrdersPageC100();
+    await this.draftConsentOrderC100();
+    await this.permissionsPageC100();
+    await this.briefDetailsPageC100();
+    await this.checkYourAnswersPageC100();
+  },
+  
+  async typeOfApplicationEventFL401() {
+    await this.actionTypeOfApplicationEvent();
+    await this.ordersApplyingForPageFL401();
+    await this.linkToChildArrangementsApplicationFL401();
+    await this.checkYourAnswersPageFL401();
   }
-
 };
