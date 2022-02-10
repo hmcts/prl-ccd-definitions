18c6c85f1a77089fd3514304061771130f902931
Jahnavi <jahnavi1satya@gmail.com>
2022-02-10 11:55:13 +0000

    Update TypeOfApplication.js

--- test/end-to-end/tests/TypeOfApplication.js
+++ test/end-to-end/tests/TypeOfApplication.js
@@ -5,3 +5,10 @@ Scenario('Create a case and Submit the Type of Application workflow @smoke-tests
   await I.createCase();
   await I.typeOfApplicationEvent();
 }).retry({ retries: 3, minTimeout: 30000 });
+
+
+Scenario('Create an FL401 case and Submit the Type of Application Event workflow', async I => {
+  await I.loginAsSolicitor();
+  await I.createCaseFL401();
+  await I.typeOfApplicationEventFL401();
+}).retry({ retries: 3, minTimeout: 30000 });
