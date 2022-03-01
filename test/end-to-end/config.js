6e730d2acde1bf38406c2a84e58b4012e677d051
Jahnavi <jahnavi1satya@gmail.com>
2022-03-01 22:54:32 +0000

    Update config.js

--- test/end-to-end/config.js
+++ test/end-to-end/config.js
@@ -5,6 +5,16 @@ module.exports = {
     email: 'prl-e2etestsolicitor@mailinator.com',
     password: defaultPassword
   },
-
+  
+  testCourtAdminUserOne: {
+    email: 'fprl_caseworker_courtadm@mailinator.com',
+    password: defaultPassword
+  },
+  
+  testCourtAdminUserTwo: {
+    email: 'fprl_caseworker_courtadm_test@mailinator.com',
+    password: defaultPassword
+  },
+  
   baseUrl: process.env.URL || 'https://manage-case.aat.platform.hmcts.net/cases'
 };
