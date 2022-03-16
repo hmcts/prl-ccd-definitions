d1e347065971202c6779f1837f92db1982fa713e
PraveenGuntoju <praveenkumar.guntoju@hmcts.net>
2022-03-16 17:41:51 +0000

    Updated unit test cases for ccd

---
+++ test/unit/definitions/FixedLists.test.js
@@ -0,0 +1,33 @@
+const { expect } = require('chai');
+const { uniqWith } = require('lodash');
+const {
+ MEDIUM_STRING,
+ isNotEmpty,
+ isNotLongerThan,
+ noDuplicateFound
+} = require('../utils/utils');
+const { ccdData } = require('../utils/dataProvider');
+
+function assertFieldDefinitionIsValid(row) {
+ expect(row.ID).to.be.a('string').and.satisfy(isNotEmpty());
+ expect(row.ListElementCode).to.be.a('string').and.satisfy(isNotEmpty());
+ expect(row.ListElement).to.be.a('string').and.satisfy(isNotEmpty());
+}
+
+describe('FixedLists', () => {
+ context('should :', () => {
+   let uniqResult = [];
+
+   before(() => {
+     uniqResult = uniqWith(ccdData.FixedLists, noDuplicateFound);
+   });
+
+   it('not contain duplicated definitions of the same field', () => {
+     expect(uniqResult).to.equal(uniqResult);
+   });
+
+   it('should have only valid definitions', () => {
+     uniqResult.forEach(assertFieldDefinitionIsValid);
+   });
+ });
+});
--- test/unit/utils/dataProvider.js
+++ test/unit/utils/dataProvider.js
@@ -1,21 +1,43 @@
 const load = require;
+const fs = require('fs');
+const path = require('path');
+const { setEnvironmentData } = require('worker_threads');
 
 const loadFile = file => {
   return Object.assign(load(`../../../definitions/private-law/json/${file}.json`), []);
 };
 
+
+let fieldsArray = [];
+getFieldata = (filePath, fileType) => {
+  fieldsArray = [];
+  processDir(filePath, fileType);
+  return fieldsArray;
+}
+
+processDir = (filePath, fileType) => {
+  const fileNames = fs.readdirSync(path.resolve(__dirname, filePath));
+  fileNames.forEach((filename) => {
+    var current_object = path.resolve(__dirname, filePath + '/' + filename); 
+    let stat = fs.statSync(current_object);
+    if (stat.isFile()) {
+      if (filename === fileType) {
+        let content = Object.assign(load(current_object), []);
+        fieldsArray = [...fieldsArray, ...content];
+      }
+    } else if (stat.isDirectory()) {
+      processDir(current_object, fileType);
+    }
+  });
+}
+
 module.exports = {
   ccdData: {
-  //  AuthorisationCaseEvent: loadFile('AuthorisationCaseEvent'),
-    //    AuthorisationCaseField: loadFile('AuthorisationCaseField'),
     AuthorisationCaseState: loadFile('AuthorisationCaseState'),
     AuthorisationCaseType: loadFile('AuthorisationCaseType'),
     CaseEvent: loadFile('CaseEvent'),
-    // CaseEventToFields: loadFile('CaseEventToFields'),
-    //    CaseField: loadFile('CaseField'),
     CaseRoles: loadFile('CaseRoles'),
     CaseType: loadFile('CaseType'),
-    //    CaseTypeTab: loadFile('CaseTypeTab'),
     FixedLists: loadFile('FixedLists'),
     Jurisdiction: loadFile('Jurisdiction'),
     SearchInputFields: loadFile('SearchInputFields'),
@@ -24,5 +46,12 @@ module.exports = {
     UserProfile: loadFile('UserProfile'),
     WorkBasketInputFields: loadFile('WorkBasketInputFields'),
     WorkBasketResultFields: loadFile('WorkBasketResultFields')
-  }
+  },
+  caseFieldata: getFieldata('../../../definitions/private-law/json', 'CaseField.json'),
+  AuthorisationCaseFieldData: getFieldata('../../../definitions/private-law/json', 'AuthorisationCaseField.json'),
+  CaseEventToFieldData: getFieldata('../../../definitions/private-law/json', 'CaseEventToFields.json'),
+  CaseTypeTab: getFieldata('../../../definitions/private-law/json', 'CaseTypeTab.json'),
+  AuthorisationCaseEvent: getFieldata('../../../definitions/private-law/json', 'AuthorisationCaseEvent.json'),
+  CaseEventToComplexTypes: getFieldata('../../../definitions/private-law/json', 'CaseEventToComplexTypes.json'),
+  ComplexTypes: getFieldata('../../../definitions/private-law/json', 'ComplexTypes.json')
 };
