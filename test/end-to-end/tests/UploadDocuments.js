0f9f04bae03af7e58089d95430db40d8d2a14fe9
PraveenGuntoju <praveenkumar.guntoju@hmcts.net>
2022-03-17 20:02:12 +0000

    Update create case for upload documents

--- test/end-to-end/tests/UploadDocuments.js
+++ test/end-to-end/tests/UploadDocuments.js
@@ -2,6 +2,6 @@ Feature('Upload documents');
 
 Scenario('Upload documents event', async I => {
   await I.loginAsSolicitor();
-  await I.createCase();
+  await I.createCaseFL401();
   await I.uploadDocuments();
 }).retry({ retries: 3, minTimeout: 30000 });
