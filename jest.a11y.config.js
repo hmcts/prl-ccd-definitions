1245789eb0d2c218d0befffd8705bdb05e596a87
saravanakumar-palanichamy <saravanakumar.palanichamy@HMCTS.NET>
2022-03-21 09:39:44 +0000

    removed duplicate line

--- jest.a11y.config.js
+++ jest.a11y.config.js
@@ -1,10 +1,7 @@
 module.exports = {
   testRegex: '(/src/test/.*|\\.test)\\.(ts|js)$',
-  moduleFileExtensions: ['ts', 'js'],
   testEnvironment: 'node',
   testRunner: 'jest-circus/runner',
-  transform: {
-    '^.+\\.ts$': 'ts-jest',
-  },
-  moduleFileExtensions: ['ts', 'js', 'json'],
+  transform: { '^.+\\.ts$': 'ts-jest' },
+  moduleFileExtensions: ['ts', 'js', 'json']
 };
