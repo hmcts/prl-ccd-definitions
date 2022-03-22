8bacb1a0c006d90eb2c0260ccad83a847e9113e9
saravanakumar-palanichamy <saravanakumar.palanichamy@HMCTS.NET>
2022-03-22 19:48:44 +0000

    test

---
+++ test/end-to-end/a11y/helpers/a11yHelper.js
@@ -0,0 +1,26 @@
+// const Helper = codecept_helper;
+//
+// class MyCustomHelper extends Helper {
+//  async getCurrentUrl() {
+//    const { browser } = this.helpers.Puppeteer;
+//    browser.ta;
+//    await browser.pages(); // List of pages in the browser
+//
+//    const { page } = this.helpers.Puppeteer;
+//    const url60 = await page.url(); // Get the url of the current page
+//    console.log(`url cool  ==> ${url60}`);
+//
+//    const helper = await this.helpers.Puppeteer;
+//    const url = await helper.page.url;
+//    console.log(`url   ==> ${url}`);
+//
+//    const currentPage = this.helpers.Puppeteer.page;
+//    currentPage.sc;
+//    const url21 = currentPage.$$('#element'); // Get the url of the current page
+//    // console.log("url21   ==> " + url21);
+//
+//    return url60;
+//  }
+// }
+//
+// module.exports = MyCustomHelper;
