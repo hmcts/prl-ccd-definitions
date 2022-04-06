f0420daf209186d157ebef221b95fcfe2b1a37fa
PraveenGuntoju <praveenkumar.guntoju@hmcts.net>
2022-04-06 10:53:22 +0100

    Updated full functional log

--- test/end-to-end/codecept.conf.js
+++ test/end-to-end/codecept.conf.js
@@ -27,6 +27,30 @@ exports.config = {
   },
   include: { I: './steps_file.js' },
   bootstrap: null,
-  mocha: {},
+  mocha: {
+    reporterOptions: {
+          'codeceptjs-cli-reporter': {
+            stdout: '-',
+            options: {
+              verbose: true,
+              steps: true
+            }
+          },
+          mochawesome: {
+            stdout: './full-functional-output/console.log',
+            options: {
+              reportDir: './full-functional-output',
+              reportFilename: 'report'
+            }
+          },
+          'mocha-junit-reporter': {
+            stdout: './full-functional-output/console.log',
+            options: {
+              mochaFile: './ffull-functional-output/result.xml',
+              attachments: 'true'
+            }
+          }
+    }
+  },
   name: 'prl-ccd-definitions'
 };
