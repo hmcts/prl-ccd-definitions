7e191a4c53223b187f1b1c349fcf5eb696b9be3d
Jahnavi <jahnavi1satya@gmail.com>
2022-04-20 08:38:08 +0100

    Update Login.js

--- test/end-to-end/pages/Login.js
+++ test/end-to-end/pages/Login.js
@@ -21,6 +21,8 @@ module.exports = {
       await I.retry(retryCount).click('#cookie-accept-all-success-banner-hide');
       await I.runAccessibilityTest();
       await I.retry(retryCount).seeElement('#authorizeCommand');
+      await I.retry(retryCount).fillField(this.fields.email, config.legalProfessionalUserOne.email);
+      await I.retry(retryCount).fillField(this.fields.password, config.legalProfessionalUserOne.password);
     } 
     catch {
       await I.retry(retryCount).fillField(this.fields.email, config.legalProfessionalUserOne.email);
