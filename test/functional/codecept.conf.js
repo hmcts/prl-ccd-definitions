4ceccca72e749d8f018dd52a39d9e3919b77ff83
pete-lowe <petemlowe@gmail.com>
2022-04-22 09:32:56 +0100

    Added field for sorting

--- test/functional/codecept.conf.js
+++ test/functional/codecept.conf.js
@@ -10,29 +10,29 @@ exports.config = {
   include: { I: './steps_file.js' },
   bootstrap: null,
   mocha: {
-      reporterOptions: {
-        'codeceptjs-cli-reporter': {
-          stdout: '-',
-          options: {
-            verbose: true,
-            steps: true
-          }
-        },
-        mochawesome: {
-          stdout: './functional-output/console.log',
-          options: {
-            reportDir: './functional-output',
-            reportFilename: 'report'
-          }
-        },
-        'mocha-junit-reporter': {
-          stdout: './functional-output/console.log',
-          options: {
-            mochaFile: './functional-output/result.xml',
-            attachments: 'true'
-          }
+    reporterOptions: {
+      'codeceptjs-cli-reporter': {
+        stdout: '-',
+        options: {
+          verbose: true,
+          steps: true
+        }
+      },
+      mochawesome: {
+        stdout: './functional-output/console.log',
+        options: {
+          reportDir: './functional-output',
+          reportFilename: 'report'
+        }
+      },
+      'mocha-junit-reporter': {
+        stdout: './functional-output/console.log',
+        options: {
+          mochaFile: './functional-output/result.xml',
+          attachments: 'true'
         }
       }
+    }
   },
   name: 'prl-ccd-definitions'
 };
