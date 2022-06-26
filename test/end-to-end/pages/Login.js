de5ed7582ec85e2d131f10502d2b20b5ca8ce41a
Jahnavi <jahnavi1satya@gmail.com>
2022-06-26 15:34:49 +0100

    Add court admin logins

--- test/end-to-end/pages/Login.js
+++ test/end-to-end/pages/Login.js
@@ -28,5 +28,38 @@ module.exports = {
       await I.retry(retryCount).fillField(this.fields.password, config.legalProfessionalUserOne.password);
     }
     await I.retry(retryCount).click(this.fields.submit);
+  },
+  
+    async loginAsCourtAdminUserOne() {
+      
+    await I.retry(retryCount).amOnPage(`${process.env.XUI_WEB_URL}`);
+    try {
+      await I.retry(retryCount).click('#cookie-accept-submit');
+      await I.retry(retryCount).click('#cookie-accept-all-success-banner-hide');
+      await I.runAccessibilityTest();
+      await I.retry(retryCount).seeElement('#authorizeCommand');
+      await I.fillField(this.fields.email, config.testCourtAdminUserOne.email);
+      await I.fillField(this.fields.password, config.testCourtAdminUserOne.password);
+    } catch {
+      await I.retry(retryCount).fillField(this.fields.email, config.legalProfessionalUserOne.email);
+      await I.retry(retryCount).fillField(this.fields.password, config.legalProfessionalUserOne.password);
+    }
+    await I.retry(retryCount).click(this.fields.submit);  
+  },
+  
+  async loginAsCourtAdminUserTwo() {
+    await I.retry(retryCount).amOnPage(`${process.env.XUI_WEB_URL}`);
+    try {
+      await I.retry(retryCount).click('#cookie-accept-submit');
+      await I.retry(retryCount).click('#cookie-accept-all-success-banner-hide');
+      await I.runAccessibilityTest();
+      await I.retry(retryCount).seeElement('#authorizeCommand');
+      await I.fillField(this.fields.email, config.testCourtAdminUserTwo.email);
+      await I.fillField(this.fields.password, config.testCourtAdminUserTwo.password);
+    } catch {
+      await I.fillField(this.fields.email, config.testCourtAdminUserTwo.email);
+      await I.fillField(this.fields.password, config.testCourtAdminUserTwo.password);
+    }
+    await I.retry(retryCount).click(this.fields.submit);
   }
 };
