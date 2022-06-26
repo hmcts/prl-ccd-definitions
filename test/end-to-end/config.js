b1f0ff8f2559df91e6da9507b00701a2f76a7831
Jahnavi <jahnavi1satya@gmail.com>
2022-06-26 15:25:26 +0100

    Update config.js

--- test/end-to-end/config.js
+++ test/end-to-end/config.js
@@ -5,13 +5,24 @@ module.exports = {
     email: 'prl-e2etestsolicitor@mailinator.com',
     password: defaultPassword
   },
+  
+  testCourtAdminUserOne: {
+    email: 'fprl_caseworker_courtadm@mailinator.com',
+    password: defaultPassword
+  },
+  
+  testCourtAdminUserTwo: {
+    email: 'fprl_caseworker_courtadm_test@mailinator.com',
+    password: defaultPassword
+  },
+  
   definition: {
     jurisdiction: 'PRIVATELAW',
     jurisdictionFullDesc: 'Family Private Law',
     caseType: 'PRLAPPS',
     caseTypeFullDesc: 'C100 & FL401 Applications'
   },
-
+  
   baseUrl: process.env.URL || 'https://manage-case.aat.platform.hmcts.net/cases',
   TestOutputDir: process.env.E2E_OUTPUT_DIR || './output',
   TestForAccessibility: process.env.TESTS_FOR_ACCESSIBILITY || false
