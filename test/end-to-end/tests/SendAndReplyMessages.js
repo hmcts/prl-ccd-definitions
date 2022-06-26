43ee232cb0e54274992debec1848406c3996c2b3
Jahnavi <jahnavi1satya@gmail.com>
2022-06-26 16:42:14 +0100

    Update SendAndReplyMessages.js

--- test/end-to-end/tests/SendAndReplyMessages.js
+++ test/end-to-end/tests/SendAndReplyMessages.js
@@ -2,8 +2,17 @@ Feature('Send and Reply to messages via case event, as different users');
 
 Scenario('Create a Case as a Solicitor, Send and Reply to Messages via Case Event as Court Admin Users', async I => {
   await I.loginAsSolicitor();
-  await I.createCase();
+  await I.createCaseFL401();
   let createdCaseUrl = await I.getCurrentPageUrl();
+  await I.typeOfApplicationEventFL401();
+  await I.runWithoutNoticeOrderHappyPath();
+  await I.applicantDetailsFL401();
+  await I.respondentDetailsFL401();
+  await I.runApplicantsFamilyEvent();
+  await I.runRelationshipToRespondent();
+  await I.runRespondentBehaviour();
+  await I.runTheHomeHappyPath();
+  I.wait('200');
   await I.signOut();
   await I.loginAsCourtAdminUserOne();
   I.wait('2');
