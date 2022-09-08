4fcc64a60e0641c484d63ae5794797434174ae24
priteshkr <pritesh.kumar@hmcts.net>
2022-09-08 15:46:55 +0100

    commented test as of now

--- src/main/java/uk/gov/hmcts/reform/prl/HighLevelDataSetupApp.java
+++ src/main/java/uk/gov/hmcts/reform/prl/HighLevelDataSetupApp.java
@@ -29,6 +29,7 @@ public class HighLevelDataSetupApp extends DataLoaderToDefinitionStore {
             new CcdRoleConfig("payments", PUBLIC),
             new CcdRoleConfig("pui-case-manager", PUBLIC),
             new CcdRoleConfig("courtnav", PUBLIC),
+            new CcdRoleConfig("caseworker-wa-task-configuration", PUBLIC),
     };
 
     private final CcdEnvironment environment;
