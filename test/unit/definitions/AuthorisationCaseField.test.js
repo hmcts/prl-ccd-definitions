48de13733faf167958d7a36201ebc3cd27c720a2
priteshkr <pritesh.kumar@hmcts.net>
2022-09-12 20:06:34 +0100

    fixed test failures

--- test/unit/definitions/AuthorisationCaseField.test.js
+++ test/unit/definitions/AuthorisationCaseField.test.js
@@ -1,13 +1,10 @@
 const { expect } = require('chai');
 const { uniqWith } = require('lodash');
 const { isFieldDuplicated } = require('../utils/utils');
-//const { createAssertExists } = require('../utils/assertBuilders');
-//const { AuthorisationCaseFieldData, caseFieldata } = require('../utils/dataProvider');
-const { AuthorisationCaseFieldData } = require('../utils/dataProvider');
+const { createAssertExists } = require('../utils/assertBuilders');
+const { AuthorisationCaseFieldData, caseFieldata } = require('../utils/dataProvider');
 
-
-
-//const assertFieldExists = createAssertExists('Field');
+const assertFieldExists = createAssertExists('Field');
 
 describe('AuthorisationCaseField', () => {
   describe('should :', () => {
@@ -16,8 +13,8 @@ describe('AuthorisationCaseField', () => {
       expect(uniqResult).to.eql(AuthorisationCaseFieldData);
     });
 
-    /*it('use existing fields', () => {
+    it('use existing fields', () => {
       assertFieldExists(AuthorisationCaseFieldData, caseFieldata);
-    });*/
+    });
   });
 });
