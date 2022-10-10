b953e1fef8f9cde80ec9c3932c33b4ec90fe81bc
priteshkr <pritesh.kumar@hmcts.net>
2022-10-10 14:02:38 +0100

    added isNotEmpty constant

--- test/unit/definitions/AuthorisationCaseType.test.js
+++ test/unit/definitions/AuthorisationCaseType.test.js
@@ -1,6 +1,7 @@
 const { expect } = require('chai');
 const { uniqWith } = require('lodash');
 const {
+  isNotEmpty,
   noDuplicateFoundACT
 } = require('../utils/utils');
 const { ccdData } = require('../utils/dataProvider');
@@ -9,7 +10,7 @@ function assertFieldDefinitionIsValid(row) {
   expect(row.CaseTypeID).to.be.a('string').and.satisfy(v => {
     return v.startsWith('PRLAPPS');
   });
-   // expect(row.UserRole).to.be.a('string').and.satisfy(isNotEmpty());
+  expect(row.UserRole).to.be.a('string').and.satisfy(isNotEmpty());
   expect(('CRUD').includes(row.CRUD)).to.eql(true);
 }
 
