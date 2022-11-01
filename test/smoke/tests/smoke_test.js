3469302b9d5f9cbfea54203ec9859736e3ac1699
REMYA MARY JAMES <RemyaMary.James@hmcts.net>
2022-11-01 17:53:03 +0000

    updated

--- test/smoke/tests/smoke_test.js
+++ test/smoke/tests/smoke_test.js
@@ -1,8 +1,8 @@
 Feature('Smoke tests @smoke-tests');
-searchResultsTopPagination = $('ccd-search-result .govuk-link');
 Scenario('Sign in as local authority and create a case', async I => {
   await I.loginAsSolicitor();
   const caseId = await I.createCaseAndReturnID();
+  const searchResultsTopPagination = $('ccd-search-result .govuk-link');
   I.navigateToCaseList();
   I.grabCurrentUrl();
   I.searchForCasesWithId(caseId);
