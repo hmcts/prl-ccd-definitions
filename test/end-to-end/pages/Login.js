5d6592aaabb9914956db0fe12aa4b86d82ac695f
Jahnavi <jahnavi1satya@gmail.com>
2022-11-08 10:13:48 +0000

    Update Login.js

--- test/end-to-end/pages/Login.js
+++ test/end-to-end/pages/Login.js
@@ -7,7 +7,6 @@ const retryCount = 3;
 const baseUrl = config.baseUrl;
 
 module.exports = {
-
   fields: {
     email: '#username',
     password: '#password',
@@ -31,15 +30,7 @@ module.exports = {
   },
   
     async loginAsCourtAdminUserOne() {
-     //remove 
-    //await I.retry(retryCount).amOnPage(`${process.env.XUI_WEB_URL}`);
     try {
-      //remove
-      // await I.retry(retryCount).click('#cookie-accept-submit');
-      // await I.retry(retryCount).click('#cookie-accept-all-success-banner-hide');
-      // await I.runAccessibilityTest();
-      // await I.retry(retryCount).seeElement('#authorizeCommand');
-// till here
       await I.fillField(this.fields.email, config.testCourtAdminUserOne.email);
       await I.fillField(this.fields.password, config.testCourtAdminUserOne.password);
     } catch {
