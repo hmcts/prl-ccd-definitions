8b3acf97a4389b1f86de378aa904c13320848086
Aloknath Datta <alokdatta.uit@gmail.com>
2023-01-01 20:25:14 +0000

    Update authorisation

--- test/functional/admin_test.js
+++ test/functional/admin_test.js
@@ -20,7 +20,6 @@ Scenario('add all the roles @pipeline', I => {
   I.createRole('caseworker-wa-task-configuration');
   I.createRole('caseworker-ras-validation');
   I.createRole('GS_profile');
-  I.createRole('caseworker-privatelaw-cafcass');
   I.click('Manage User Roles');
   I.see('citizen');
   I.see('caseworker-privatelaw-solicitor');
@@ -39,7 +38,6 @@ Scenario('add all the roles @pipeline', I => {
   I.see('caseworker-wa-task-configuration');
   I.see('caseworker-ras-validation');
   I.see('GS_profile');
-  I.see('caseworker-privatelaw-cafcass');
 }).retry({ retries: 3, minTimeout: 30000 }); // eslint-disable-line no-magic-numbers
 
 Scenario('upload Private Law Config file @pipeline', I => {
