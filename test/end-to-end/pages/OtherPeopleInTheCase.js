1ec650464787dbe25f5075c70e5c6b4266c7c588
elizereyes123 <107991381+elizereyes123@users.noreply.github.com>
2023-02-23 10:18:09 +0000

    Updated OtherPeopleInTheCase test

--- test/end-to-end/pages/OtherPeopleInTheCase.js
+++ test/end-to-end/pages/OtherPeopleInTheCase.js
@@ -6,7 +6,8 @@ module.exports = {
     headerText: 'Other people in the case',
     textareaText: 'Testing text area',
     submit: 'button[type="submit"]',
-    otherPeopleAddress: 'othersToNotify_0_address_address',
+    otherPeoplePlaceOfBirth: '//*[@id="otherPartyInTheCaseRevised_0_placeOfBirth"]',
+    otherPeopleAddress: 'otherPartyInTheCaseRevised_0_address_address'
   },
 
   async otherPeopleInTheCase() {
@@ -14,27 +15,30 @@ module.exports = {
     await I.retry(retryCount).waitForPage('h1', this.fields.headerText);
     await I.retry(retryCount).click('Add new');
     I.wait('1');
-    await I.retry(retryCount).fillField('//input[@id="othersToNotify_0_firstName"]', 'Other Firstname');
+    await I.retry(retryCount).fillField('//*[@id="otherPartyInTheCaseRevised_0_firstName"]', 'Other Firstname');
     I.wait('2');
-    await I.retry(retryCount).fillField('//input[@id="othersToNotify_0_lastName"]', 'Other Lastname');
-    await I.retry(retryCount).fillField('//input[@id="othersToNotify_0_previousName"]', 'Other PreviousName');
-    await I.retry(retryCount).checkOption('#othersToNotify_0_isDateOfBirthKnown_Yes');
-    await I.retry(retryCount).fillField('//input[@id="dateOfBirth-day"]', '12');
-    await I.retry(retryCount).fillField('//input[@id="dateOfBirth-month"]', '11');
-    await I.retry(retryCount).fillField('//input[@id="dateOfBirth-year"]', '2007');
-    await I.retry(retryCount).checkOption('//input[@id="othersToNotify_0_gender-male"]');
-    await I.retry(retryCount).checkOption('//input[@id="othersToNotify_0_isPlaceOfBirthKnown_Yes"]');
-    await I.retry(retryCount).fillField('//input[@id="othersToNotify_0_placeOfBirth"]', this.fields.textareaText);
-    await I.retry(retryCount).checkOption('//input[@id="othersToNotify_0_isCurrentAddressKnown_Yes"]');
+    await I.retry(retryCount).fillField('//*[@id="otherPartyInTheCaseRevised_0_lastName"]', 'Other Lastname');
+    await I.retry(retryCount).fillField('//*[@id="otherPartyInTheCaseRevised_0_previousName"]', 'Other PreviousName');
+    await I.retry(retryCount).checkOption('#otherPartyInTheCaseRevised_0_isDateOfBirthKnown_Yes');
+    await I.retry(retryCount).fillField('//*[@id="dateOfBirth-day"]', '12');
+    await I.retry(retryCount).fillField('//*[@id="dateOfBirth-month"]', '11');
+    await I.retry(retryCount).fillField('//*[@id="dateOfBirth-year"]', '2007');
+    await I.retry(retryCount).checkOption('//*[@id="otherPartyInTheCaseRevised_0_gender-male"]');
+    await I.retry(retryCount).checkOption('//*[@id="otherPartyInTheCaseRevised_0_isPlaceOfBirthKnown_Yes"]');
+    await I.retry(retryCount).fillField(this.fields.otherPeoplePlaceOfBirth, this.fields.textareaText);
+    await I.retry(retryCount).checkOption('//*[@id="otherPartyInTheCaseRevised_0_isCurrentAddressKnown_Yes"]');
+    I.wait('1');
     await I.retry(retryCount).selectPostCodeLookupAddress(this.fields.otherPeopleAddress, 'B11LS');
-    await I.retry(retryCount).checkOption('//input[@id="othersToNotify_0_isAddressConfidential_Yes"]');
-    await I.retry(retryCount).checkOption('//input[@id="othersToNotify_0_canYouProvideEmailAddress_Yes"]');
-    await I.retry(retryCount).fillField('//input[@id="othersToNotify_0_email"]', 'otherstonotify@email.com');
-    await I.retry(retryCount).checkOption('//input[@id="othersToNotify_0_isEmailAddressConfidential_Yes"]');
-    await I.retry(retryCount).checkOption('//input[@id="othersToNotify_0_canYouProvidePhoneNumber_Yes"]');
-    await I.retry(retryCount).fillField('//input[@id="othersToNotify_0_phoneNumber"]', '07122884667');
-    await I.retry(retryCount).checkOption('//input[@id="othersToNotify_0_isPhoneNumberConfidential_Yes"]');
-    await I.retry(retryCount).waitForText('Save and continue', '10');
-    await I.retry(retryCount).click('Save and continue');
+    await I.retry(retryCount).checkOption('//*[@id="otherPartyInTheCaseRevised_0_isAddressConfidential_Yes"]');
+    await I.retry(retryCount).checkOption('//*[@id="otherPartyInTheCaseRevised_0_isAtAddressLessThan5Years_Yes"]');
+    await I.retry(retryCount).fillField('//*[@id="otherPartyInTheCaseRevised_0_addressLivedLessThan5YearsDetails"]', this.fields.textareaText);
+    await I.retry(retryCount).checkOption('//*[@id="otherPartyInTheCaseRevised_0_canYouProvideEmailAddress_Yes"]');
+    await I.retry(retryCount).fillField('//*[@id="otherPartyInTheCaseRevised_0_email"]', 'otherstonotify@email.com');
+    await I.retry(retryCount).checkOption('//*[@id="otherPartyInTheCaseRevised_0_isEmailAddressConfidential_Yes"]');
+    await I.retry(retryCount).checkOption('//*[@id="otherPartyInTheCaseRevised_0_canYouProvidePhoneNumber_Yes"]');
+    await I.retry(retryCount).fillField('//*[@id="otherPartyInTheCaseRevised_0_phoneNumber"]', '07122884667');
+    await I.retry(retryCount).checkOption('//*[@id="otherPartyInTheCaseRevised_0_isPhoneNumberConfidential_Yes"]');
+    await I.retry(retryCount).waitForText('Continue', '10');
+    await I.retry(retryCount).click('Continue');
   }
 };
