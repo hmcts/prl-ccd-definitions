3ae90ad359eead84ab939a860ff0a03e7022e00c
Aloknath Datta <alokdatta.uit@gmail.com>
2023-03-29 00:42:25 +0100

    Updated FQDN for smoke and func test

--- test/smoke/tests/smoke_test.js
+++ test/smoke/tests/smoke_test.js
@@ -2,12 +2,12 @@ Feature('Smoke tests @smoke-tests');
 Scenario('Sign in as local authority and create a case', async I => {
   await I.loginAsSolicitor();
   await I.createCaseAndReturnID();
-//  const searchResultsCaseId = 'ccd-search-result .govuk-link';
-//  I.navigateToCaseList();
-//  I.grabCurrentUrl();
-//  I.searchForCasesWithId(caseId);
-//  I.grabCurrentUrl();
-//  I.waitForElement(searchResultsCaseId);
+  const searchResultsCaseId = 'ccd-search-result .govuk-link';
+  I.navigateToCaseList();
+  I.grabCurrentUrl();
+  I.searchForCasesWithId(caseId);
+  I.grabCurrentUrl();
+  I.waitForElement(searchResultsCaseId);
   // The following test cases are no longer required as 'ccd-search-result .govuk-link' takes care of searching the case created.
   // I.grabCurrentUrl();
   // I.seeCaseInSearchResult(caseId);
