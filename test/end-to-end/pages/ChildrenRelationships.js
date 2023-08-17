d194078d78673f024d2a1e8ecf31999f29497adc
elizereyes123 <107991381+elizereyes123@users.noreply.github.com>
2023-08-17 12:33:23 +0100

    Add people feature changes

---
+++ test/end-to-end/pages/ChildrenRelationships.js
@@ -0,0 +1,59 @@
+const I = actor();
+const retryCount = 3;
+
+module.exports = {
+  fields: {
+    submit: 'button[type="submit"]',
+    textareaText: 'Testing text area',
+  },
+
+  async childrenAndApplicant() {
+    await I.retry(retryCount).triggerEvent('Children and applicants');
+    await I.retry(retryCount).waitForText('Children and applicants', 30)
+    I.wait('1');
+    await I.retry(retryCount).selectOption('//input[@id="buffChildAndApplicantRelations_0_childAndApplicantRelation"]', 'Father');
+    I.wait('1');
+    await I.retry(retryCount).checkOption('#buffChildAndApplicantRelations_0_childLivesWith_Yes');
+    I.wait('1');
+    await I.retry(retryCount).selectOption('//input[@id="buffChildAndApplicantRelations_1_childAndApplicantRelation"]', 'Father');
+    I.wait('1');
+    await I.retry(retryCount).checkOption('#buffChildAndApplicantRelations_1_childLivesWith_Yes');
+  },
+  async childrenAndRespondent() {
+    await I.retry(retryCount).triggerEvent('Children and respondents');
+    await I.retry(retryCount).waitForText('Children and respondents', 30)
+    I.wait('1');
+    await I.retry(retryCount).selectOption('//input[@id="buffChildAndRespondentRelations_0_childAndRespondentRelation"]', 'Mother');
+    I.wait('1');
+    await I.retry(retryCount).checkOption('#buffChildAndRespondentRelations_0_childLivesWith_Yes');
+    I.wait('1');
+    await I.retry(retryCount).selectOption('//input[@id="buffChildAndRespondentRelations_1_childAndRespondentRelation"]', 'Mother');
+    I.wait('1');
+    await I.retry(retryCount).checkOption('#buffChildAndRespondentRelations_1_childLivesWith_Yes');
+  },
+  async childrenAndOtherPeople() {
+    await I.retry(retryCount).triggerEvent('Children and other people');
+    await I.retry(retryCount).waitForText('Children and other people', 30)
+    I.wait('1');
+    await I.retry(retryCount).selectOption('//input[@id="buffChildAndOtherPeopleRelations_0_childAndOtherPeopleRelation"]', 'Grandparent');
+    I.wait('1');
+    await I.retry(retryCount).checkOption('#buffChildAndOtherPeopleRelations_0_childLivesWith_Yes');
+    I.wait('1');
+    await I.retry(retryCount).checkOption('#buffChildAndOtherPeopleRelations_0_isChildLivesWithPersonConfidential_Yes');
+  },
+  async runChildrenAndApplicant() {
+    await this.childrenAndApplicant();
+    await I.submitEvent();
+    await I.amOnHistoryPageWithSuccessNotification();
+  },
+  async runChildrenAndRespondent() {
+    await this.childrenAndRespondent();
+    await I.submitEvent();
+    await I.amOnHistoryPageWithSuccessNotification();
+  },
+  async runChildrenAndOtherPeople() {
+    await this.childrenAndOtherPeople();
+    await I.submitEvent();
+    await I.amOnHistoryPageWithSuccessNotification();
+  }
+};
--- test/end-to-end/pages/OtherPeopleInTheCase.js
+++ test/end-to-end/pages/OtherPeopleInTheCase.js
@@ -11,8 +11,7 @@ module.exports = {
 
   async otherPeopleInTheCase() {
     await I.retry(retryCount).triggerEvent(this.fields.headerText);
-    await I.retry(retryCount).waitForPage('h1', this.fields.headerText);
-    await I.retry(retryCount).click('Add new');
+    await I.retry(retryCount).waitForText('h1', 'Children and applicants');
     I.wait('1');
     await I.retry(retryCount).fillField('//input[@id="othersToNotify_0_firstName"]', 'Other Firstname');
     I.wait('2');
@@ -34,9 +33,6 @@ module.exports = {
     await I.retry(retryCount).checkOption('//input[@id="othersToNotify_0_canYouProvidePhoneNumber_Yes"]');
     await I.retry(retryCount).fillField('//input[@id="othersToNotify_0_phoneNumber"]', '07122884667');
     await I.retry(retryCount).checkOption('//input[@id="othersToNotify_0_isPhoneNumberConfidential_Yes"]');
-    await I.retry(retryCount).click('div#othersToNotify_0_otherPersonRelationshipToChildren  .button.write-collection-add-item__top');
-    I.wait('2');
-    await I.retry(retryCount).fillField('//input[@id="othersToNotify_0_otherPersonRelationshipToChildren_0_personRelationshipToChild"]', this.fields.textareaText);
     await I.retry(retryCount).click('Continue');
     await I.retry(retryCount).waitForText('Save and continue', '10');
     await I.retry(retryCount).click('Save and continue');
