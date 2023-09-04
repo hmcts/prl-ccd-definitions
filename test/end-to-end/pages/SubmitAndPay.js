5ba18200abe3ff749f86cf5f9c00bfae87143527
elizereyes123 <107991381+elizereyes123@users.noreply.github.com>
2023-09-04 15:50:12 +0100

    Add new ChildrenRelationships

--- test/end-to-end/pages/SubmitAndPay.js
+++ test/end-to-end/pages/SubmitAndPay.js
@@ -9,35 +9,59 @@ module.exports = {
   },
 
   async confidentialityStatement() {
-    I.wait('10');
+    await I.wait('10');
     await I.retry(retryCount).waitForText('Confidentiality Statement');
-    I.wait('1');
+    await I.wait('1');
     await I.retry(retryCount).click('#confidentialityDisclaimer_confidentialityChecksChecked-confidentialityChecksChecked');
     await I.retry(retryCount).click('Continue');
   },
 
   async declaration() {
-    I.wait('2');
+    await I.wait('2');
     await I.retry(retryCount).waitForText('Declaration');
     await I.retry(retryCount).wait('1');
     await I.retry(retryCount).click('#payAgreeStatement-agree');
-    I.wait('1');
+    await I.wait('1');
     await I.retry(retryCount).click('Continue');
   },
 
   async payNow() {
-    I.wait('2');
-    await I.retry(retryCount).click('Continue');
-    I.wait('2');
-    await I.retry(retryCount).click('Pay now');
-    I.wait('5');
+    await I.wait('2');
+    await I.retry(retryCount).click(this.fields.submit);
   },
 
+  async happensNext() {
+    await I.wait('6');
+    await I.retry(retryCount).click('Close and Return to case details');
+    await I.wait('4');
+    await I.retry(retryCount).waitForText('Case Status');
+    await I.wait('2');
+    await I.retry(retryCount).waitForText('Pending');
+  },
+
+  async runDummyPayment() {
+    await I.wait('4');
+    await I.retry(retryCount).triggerEvent('Dummy Payment confirmation');
+    await I.wait('4');
+    await I.retry(retryCount).click('Make the payment');
+    await I.wait('6');
+  },
+
+  async caseSubmittedCA() {
+    await I.wait('4');
+    await I.retry(retryCount).waitForText('Case Status');
+    await I.wait('2');
+    await I.retry(retryCount).waitForText('Submitted');
+  },
+  
   async submitAndPay() {
     await this.triggerEvent();
     await this.confidentialityStatement();
     await this.declaration();
     await this.payNow();
-    // await I.retry(retryCount).amOnHistoryPageWithSuccessNotification();
+    await this.happensNext();
+    await this.runDummyPayment();
+    await I.retry(retryCount).amOnHistoryPageWithSuccessNotification();
+    await this.caseSubmittedCA();
   }
 };
