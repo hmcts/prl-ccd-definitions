9166c4737feffa2aa7e42b7e6a7ea3a6c78a1386
elizereyes123 <107991381+elizereyes123@users.noreply.github.com>
2023-09-04 17:42:34 +0100

    extend wait time for happensNext

--- test/end-to-end/pages/SubmitAndPay.js
+++ test/end-to-end/pages/SubmitAndPay.js
@@ -31,7 +31,7 @@ module.exports = {
   },
 
   async happensNext() {
-    await I.wait('6');
+    await I.wait('12');
     await I.retry(retryCount).click('Close and Return to case details');
     await I.wait('4');
     await I.retry(retryCount).waitForText('Case Status');
