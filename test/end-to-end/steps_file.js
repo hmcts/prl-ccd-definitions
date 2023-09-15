5450df223067de2a78831436e435bbca6b0d91c5
Bala <bala@Balas-MacBook-Pro.local>
2023-09-15 20:11:09 +0100

    judge approve flow initial

--- test/end-to-end/steps_file.js
+++ test/end-to-end/steps_file.js
@@ -31,6 +31,7 @@ const manageOrders = require('./pages/ManageOrders');
 const manageOrderHearing = require('./pages/ManageOrderHearing');
 const OtherChildrenNotInCase = require('./pages/OtherChildrenNotInCase');
 const ChildrenRelationships = require('./pages/ChildrenRelationships');
+const editAndApproveDraftOrder = require('./pages/EditAndApproveDraftOrder');
 
 module.exports = () => {
   return actor({
@@ -39,6 +40,12 @@ module.exports = () => {
     },
     loginAsCourtAdmin() {
       return LoginPage.loginAsCourtAdmin();
+    },
+    loginAsJudge() {
+      return LoginPage.loginAsJudge();
+    },
+    selectOrderForReview() {
+      return editAndApproveDraftOrder.selectOrderForJudgeReview();
     },
     runAttendingTheHearingEvent() {
       return AttendingTheHearing.runEventHappyPathAttendingTheHearing();
--- test/end-to-end/tests/CAManageOrderUploadOrder.js
+++ test/end-to-end/tests/CAManageOrderUploadOrder.js
@@ -11,3 +11,8 @@ Scenario('CA Manage Order Upload Order - For Judge review - Personally @nightly'
   await I.createC100CaseByCourtAdmin();
   await I.manageOrderUploadOrderForJudgeReview();
 }).retry({ retries: 3, minTimeout: 30000 });
+
+Scenario('Judge Edit and approve an order @nightly', async I => {
+  await I.loginAsJudge();
+  await I.selectOrderForJudgeReview();
+}).retry({ retries: 3, minTimeout: 30000 });
