6b3ad575729cd28ec669cf46f262144b9fbffd89
Bala <bala@Balas-MacBook-Pro.local>
2023-09-18 18:59:36 +0100

    updated the test

--- test/end-to-end/tests/CAManageOrderUploadOrder.js
+++ test/end-to-end/tests/CAManageOrderUploadOrder.js
@@ -6,13 +6,8 @@ Scenario('CA Manage Order Upload Order Serve order Personally @nightly', async I
   await I.manageOrderUploadOrderServeNowPersonally();
 }).retry({ retries: 3, minTimeout: 30000 });
 
-Scenario('CA Manage Order Upload Order - For Judge review - Personally @nightly', async I => {
+Scenario('CA Manage Order Upload Order - For Judge review - @nightly', async I => {
   await I.loginAsCourtAdmin();
   await I.createC100CaseByCourtAdmin();
   await I.manageOrderUploadOrderForJudgeReview();
-}).retry({ retries: 3, minTimeout: 30000 });
-
-Scenario('Judge Edit and approve an order @nightly', async I => {
-  await I.loginAsJudge();
-  await I.selectOrderForJudgeReview();
 }).retry({ retries: 3, minTimeout: 30000 });
