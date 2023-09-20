3ad3957c4ac9fda745fa1b49fa9bfc89914b9357
Aloknath Datta <Aloknath.datta@hmcts.net>
2023-09-20 15:25:17 +0100

    Update CreateCase.js

--- test/end-to-end/pages/CreateCase.js
+++ test/end-to-end/pages/CreateCase.js
@@ -27,7 +27,7 @@ module.exports = {
     I.wait('20');
     await I.waitForElement(this.fields.jurisdiction);
     await I.retry(retryCount).selectOption(this.fields.jurisdiction, 'Family Private Law');
-    I.wait(10');
+    I.wait('10');
     await I.retry(retryCount).selectOption(this.fields.caseType, 'C100 & FL401 Applications');
     I.wait('10');
     await I.retry(retryCount).selectOption(this.fields.event, 'Solicitor application');
