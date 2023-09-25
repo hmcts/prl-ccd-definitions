6d87a41840fa7ee4942eee54c19601083464f7b4
harshniavadhannla <harshni.avadhannla@hmcts.net>
2023-09-25 15:58:29 +0100

    changed to previous state

--- test/end-to-end/tests/SolicitorHWFinAWP.js
+++ test/end-to-end/tests/SolicitorHWFinAWP.js
@@ -1,6 +1,6 @@
 Feature('Help with Fee in AWP - TS Solicitor application');
 
-Scenario('Solicitor Help With Fee in AWP @nightly', async({ I }) => {
+Scenario('Solicitor Help With Fee in AWP @nightly', async I => {
   await I.loginAsSolicitor();
   await I.createCase_TS();
   await I.runSubmitAndPayHappyPath();
--- test/end-to-end/tests/SolicitorHelpWithFeeYESErrorMsg.js
+++ test/end-to-end/tests/SolicitorHelpWithFeeYESErrorMsg.js
@@ -2,7 +2,7 @@ Feature('Help with Fee - Solicitor - CA Submit - Error message');
 
 Scenario(
   'Solicitor Help With Fee - YES option - Create CA Submit -  Error message @nightly',
-  async({ I }) => {
+  async I => {
     await I.loginAsSolicitor();
     await I.createCase_TS();
     await I.runSubmitAndPayHappyPath_HWF_Yes();
