1a0c494b3281bb0a05d00dff8cf10043b052ca85
harshniavadhannla <harshni.avadhannla@hmcts.net>
2023-09-25 14:13:57 +0100

    test

--- test/end-to-end/tests/SubmitAndPayDO.js
+++ test/end-to-end/tests/SubmitAndPayDO.js
@@ -1,14 +1,17 @@
 Feature('Submit and Pay - DO');
 
-Scenario('Submit and Pay - basic journey for FL401 Casetype @nightly @master', async I => {
-  await I.loginAsSolicitor();
-  await I.createCaseFL401();
-  await I.typeOfApplicationEventFL401();
-  await I.runWithoutNoticeOrderHappyPath();
-  await I.applicantDetailsFL401();
-  await I.respondentDetailsFL401();
-  await I.runApplicantsFamilyEvent();
-  await I.runRelationshipToRespondent();
-  await I.runRespondentBehaviour();
-  await I.runTheHomeHappyPath();
-}).retry({ retries: 3, minTimeout: 30000 });
+Scenario(
+  'Submit and Pay - basic journey for FL401 Casetype @nightly',
+  async({ I }) => {
+    await I.loginAsSolicitor();
+    await I.createCaseFL401();
+    await I.typeOfApplicationEventFL401();
+    await I.runWithoutNoticeOrderHappyPath();
+    await I.applicantDetailsFL401();
+    await I.respondentDetailsFL401();
+    await I.runApplicantsFamilyEvent();
+    await I.runRelationshipToRespondent();
+    await I.runRespondentBehaviour();
+    await I.runTheHomeHappyPath();
+  }
+).retry({ retries: 3, minTimeout: 30000 });
