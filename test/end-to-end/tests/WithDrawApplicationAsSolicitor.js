db492ee165192c2d6c021a38d027bec6df0ab73f
harshniavadhannla <harshni.avadhannla@hmcts.net>
2023-09-26 09:24:14 +0100

    reverted to old version

--- test/end-to-end/tests/WithDrawApplicationAsSolicitor.js
+++ test/end-to-end/tests/WithDrawApplicationAsSolicitor.js
@@ -4,5 +4,6 @@ Scenario('Withdraw application as Solicitor @cross-browser', async I => {
   await I.loginAsSolicitor();
   await I.createSolicitorDummyCase();
   await I.payAndSubmitDummySolicitorCase();
+
   await I.solicitorWithdrawApplication();
 }).retry({ retries: 3, minTimeout: 30000 });
