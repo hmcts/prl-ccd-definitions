675b27626a2064356eec1a7c0f02e4ce427e6752
gokul-sol <gokul.sridharan@HMCTS.NET>
2023-10-03 10:03:13 +0100

    Remove cross browser tags for this test

--- test/end-to-end/tests/GateKeepingState.js
+++ test/end-to-end/tests/GateKeepingState.js
@@ -1,6 +1,6 @@
 Feature('Move case to Gatekeeping State');
 
-Scenario('Move case to Gatekeeping State @cross-browser  @master', async I => {
+Scenario('Move case to Gatekeeping State @master', async I => {
   await I.loginAsSolicitor();
   await I.createSolicitorDummyCase();
   await I.payAndSubmitDummySolicitorCase();
