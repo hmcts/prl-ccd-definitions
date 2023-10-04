f883775e659d98aa9882fa0c37e00199275ffec5
Bala <bala@Balas-MacBook-Pro.local>
2023-10-04 12:17:22 +0100

    included master tag

--- test/end-to-end/tests/CAGenerateRespondentC8.js
+++ test/end-to-end/tests/CAGenerateRespondentC8.js
@@ -1,7 +1,7 @@
 Feature('Court Admin / Judge / LA Amend Respondent details - Email, Address, Phone number - Confidential - C8 Generation - TS Solicitor Application');
 
 Scenario(
-  'Amend Respondent Email, Address, Phone number as Confidential @nightly',
+  'Amend Respondent Email, Address, Phone number as Confidential @master',
   async I => {
     await I.loginAsCourtAdmin();
     await I.createC100CaseByCourtAdmin();
@@ -10,7 +10,7 @@ Scenario(
 ).retry({ retries: 3, minTimeout: 30000 });
 
 Scenario(
-  'Judge - Amend Respondent Email, Address, Phone number as Confidential @nightly',
+  'Judge - Amend Respondent Email, Address, Phone number as Confidential @master',
   async I => {
     await I.loginAsCourtAdmin();
     await I.createC100CaseByCourtAdmin();
@@ -20,7 +20,7 @@ Scenario(
 ).retry({ retries: 3, minTimeout: 30000 });
 
 Scenario(
-  'Legal Adviser - Amend Respondent Email, Address, Phone number as Confidential @nightly',
+  'Legal Adviser - Amend Respondent Email, Address, Phone number as Confidential @master',
   async I => {
     await I.loginAsCourtAdmin();
     await I.createC100CaseByCourtAdmin();
