dca452b1600a3ccb330523960f1d89894d65b38c
gokul-sol <gokul.sridharan@HMCTS.NET>
2023-10-18 16:01:36 +0100

    initiate nightly build in jenkins

---
+++ test/end-to-end/tests/CreateHearings_test.js
@@ -0,0 +1,10 @@
+const testConfig = require('../config');
+
+Feature('Create hearings for a case');
+Scenario('Create manual hearing for a case @wip', async({ I }) => {
+  await I.loginAsSwanseaCourtAdmin();
+  await I.createFLCaseByCourtAdmin();
+  await I.manageOrderCreateOrderC21();
+//   await I.fillHearingDetails();
+//   await I.submitManageOrder();
+}).retry(testConfig.TestRetryScenarios);
