bcc77a08cc2323bf4713919dbc4d4c9ebf2b6c04
gokul-sol <gokul.sridharan@HMCTS.NET>
2023-11-20 14:42:47 +0000

    revert exui image and add audio files tests

--- test/end-to-end/pages/ManageDocumentsScreens/ManageDocuments.js
+++ test/end-to-end/pages/ManageDocumentsScreens/ManageDocuments.js
@@ -5,6 +5,8 @@ const manageDocConfig = require('./manageDocConfig');
 
 const date = new Date();
 const formattedDate = date.toLocaleDateString('en-GB', { day: 'numeric', month: 'short', year: 'numeric' }).replace(/ /g, ' ');
+let pdfDocFilePath = '../resource/dummy.pdf';
+let audioDocFilePath = '../resource/test_av.mp3';
 
 module.exports = {
 
@@ -25,7 +27,14 @@ module.exports = {
     confFileEle: '//span[contains(text(), "dummy.pdf")]',
     noticeOfHearingFolderEle: '//*[@aria-level="2"]/button/span[contains(text(), "Notice of Hearing")]',
     noticeOfHearingFileEle: '//*[@aria-level="3"]/button/span[contains(text(), "dummy.pdf")]',
-    nextBtnSelector: '.mat-tab-header-pagination-after  .mat-tab-header-pagination-chevron'
+    nextBtnSelector: '.mat-tab-header-pagination-after  .mat-tab-header-pagination-chevron',
+    
+    // add new fields
+    additionalSelectParty: '#manageDocuments_1_documentParty',
+    additionalDocCategoryField: '#manageDocuments_1_documentCategories',
+    additionalDocUploadField: '#manageDocuments_1_document',
+    additionalDocDetailsField: '#manageDocuments_1_documentDetails',
+    additionalSelectRestrictAccess: '#manageDocuments_1_documentRestrictCheckbox-restrictToGroup',
   },
 
   async triggerEvent() {
@@ -46,6 +55,25 @@ module.exports = {
     await I.click(this.fields.selectRestrictAccess);
   },
 
+  async addAudioDocuments(documentCategory, documentDetails) {
+    const uploadTime = 5;
+    await I.waitForElement(this.fields.additionalSelectParty);
+    await I.selectOption(this.fields.additionalSelectParty, manageDocConfig.partyType);
+    await I.selectOption(this.fields.additionalDocCategoryField, documentCategory);
+    await I.attachFile(this.fields.additionalDocUploadField, '../resource/test_av.mp3');
+    await I.wait(uploadTime);
+    await I.fillField(this.fields.additionalDocDetailsField, documentDetails);
+  },
+
+  async applyRestrictAccessForAudioDoc() {
+    await I.click(this.fields.additionalSelectRestrictAccess);
+  },
+
+  async addNewDocuments() {
+    await I.click('Add new');
+    await I.waitForVisible(this.fields.additionalSelectParty);
+  },
+
   async submitDocuments() {
     await I.click(this.fields.submit);
     await I.waitForText(manageDocConfig.cyaText);
@@ -60,12 +88,17 @@ module.exports = {
 
     await I.click(this.fields.documentReviewTab);
     await I.see('Yes, restrict access.');
+    await I.see(manageDocConfig.pdfFileName);
     await I.see(manageDocConfig.docCategory);
     await I.see(manageDocConfig.docDetailsText);
     await I.see(formattedDate);
+
+    //verify audio file submission
+    await I.see(manageDocConfig.audioFileName);
+    await I.see('Court staff uploaded documents 2');
   },
 
-  async triggerReviewDocEvent() {
+  async triggerReviewDocEventForFiles(fileName) {
     await I.triggerEvent('Review documents');
     await I.waitForText(manageDocConfig.reviewDocText);
 
@@ -73,6 +106,7 @@ module.exports = {
     await I.selectOption(this.fields.selectReviewDoc, option);
     await I.click(this.fields.submit);
     await I.waitForText(manageDocConfig.docCategory);
+    await I.waitForText(fileName);
     await I.click(this.fields.restrictDocEle);
     await I.click(this.fields.submit);
 
@@ -92,6 +126,10 @@ module.exports = {
     await I.see(manageDocConfig.docDetailsText);
     await I.see(manageDocConfig.partyType);
     await I.see(formattedDate);
+
+    //verify audio files
+    await I.see('Court staff uploaded confidential documents 2');
+    await I.see(manageDocConfig.audioFileName);
   },
 
   async verifyCaseFileView() {
@@ -123,14 +161,20 @@ module.exports = {
     await this.triggerEvent();
     await this.mainApplicationDocuments(manageDocConfig.docCategory, manageDocConfig.docDetailsText);
     await this.applyRestrictAccess();
+
+    await this.addNewDocuments();
+    await this.addAudioDocuments(manageDocConfig.docCategory, manageDocConfig.docDetailsText);
+    await this.applyRestrictAccessForAudioDoc();
+
     await this.submitDocuments();
     await I.amOnHistoryPageWithSuccessNotification();
     await this.verifyDocumentSubmission();
   },
 
   async reviewDocuments() {
-    await this.triggerReviewDocEvent();
+    await this.triggerReviewDocEventForFiles(manageDocConfig.pdfFileName);
     await I.amOnHistoryPageWithSuccessNotification();
+    await this.triggerReviewDocEventForFiles(manageDocConfig.audioFileName);
     await this.verifyReviewDocSubmission();
     await this.verifyCaseFileView();
   },
