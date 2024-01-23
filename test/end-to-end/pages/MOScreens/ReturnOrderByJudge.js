853e1cdeddc3a8e00c64fc2aae195d76733e5139
Jahnavi <jahnavi1satya@gmail.com>
2024-01-23 10:56:26 +0000

    Create ReturnOrderByJudge.js

---
+++ test/end-to-end/pages/MOScreens/ReturnOrderByJudge.js
@@ -0,0 +1,27 @@
+const I = actor();
+const retryCount = 3;
+
+module.exports = {
+
+  fields: {
+    draftOrdersDynamicList: '#draftOrdersDynamicList',
+    draftOrdersDynamicListOption: '//select[@id="draftOrdersDynamicList"]/option[2]'
+  },
+
+    async editAReturnedOrderByJudge() {
+
+        await I.retry(retryCount).triggerEvent('Edit a returned order');
+        await I.retry(retryCount).waitForText('Select the order');
+        const option = await I.grabTextFrom(this.fields.draftOrdersDynamicListOption);
+        await I.selectOption(this.fields.draftOrdersDynamicList,option);
+        await I.retry(retryCount).click('Continue');
+        await I.wait('90');//2');
+        
+        await I.retry(retryCount).waitForText('Instructions from the judge');
+        //'Reject solicitor draft order'
+        await I.retry(retryCount).click('Continue');
+        await I.wait('90');//2');
+}
+};
+
+
