e1195ceb14cfbd085530fb2dd41e9ab4d1ed005a
Jahnavi <jahnavi1satya@gmail.com>
2024-01-23 16:09:41 +0000

    Update EditAndApproveDraftOrder.js

--- test/end-to-end/pages/EditAndApproveDraftOrder.js
+++ test/end-to-end/pages/EditAndApproveDraftOrder.js
@@ -7,8 +7,19 @@ module.exports = {
     orderByConsent_Yes: '#isTheOrderByConsent_Yes',
     draftOrdersDynamicList: '#draftOrdersDynamicList',
     draftOrdersDynamicListOption: '//select[@id="draftOrdersDynamicList"]/option[2]',
-    instructionsToLegalRepresentative: '//input[@id="instructionsToLegalRepresentative"]'
+    instructionsToLegalRepresentative: '//input[@id="instructionsToLegalRepresentative"]',
+    tabSelector: '//div[contains(text(), "Draft orders")]',
+    nextBtnSelector: '.mat-tab-header-pagination-after  .mat-tab-header-pagination-chevron'
   },
+
+  async verifyDraftOrderSubmission() {
+    await I.wait('2');
+    await I.clickTillElementFound(this.fields.tabSelector, this.fields.nextBtnSelector);
+    await I.click(this.fields.tabSelector);
+    await I.runAccessibilityTest();
+    await I.waitForText('Rejected by Judge');
+  },
+
   async selectOrderForJudgeReview() {
     await I.retry(retryCount).triggerEvent('Edit and approve a draft order');
     await I.wait('2');
@@ -25,7 +36,13 @@ module.exports = {
     await I.retry(retryCount).fillField(this.fields.instructionsToLegalRepresentative,'Reject solicitor draft order');
     await I.retry(retryCount).click('Continue');
     await I.wait('2');
+
     await I.retry(retryCount).waitForText('Check your answers');
     await I.retry(retryCount).click('Submit');
+    await I.wait('2');
+    
+    await I.retry(retryCount).click('Close and Return to case details');
+    await I.wait('2')
+    await I.retry(retryCount).verifyDraftOrderSubmission();
   }
 };
