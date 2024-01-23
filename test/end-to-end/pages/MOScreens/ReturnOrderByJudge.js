28c9264c49580b5302bc01a1e41ede3ba5de5914
Jahnavi <jahnavi1satya@gmail.com>
2024-01-23 11:57:42 +0000

    Update ReturnOrderByJudge.js

--- test/end-to-end/pages/MOScreens/ReturnOrderByJudge.js
+++ test/end-to-end/pages/MOScreens/ReturnOrderByJudge.js
@@ -9,18 +9,16 @@ module.exports = {
   },
 
     async editAReturnedOrderByJudge() {
-
         await I.retry(retryCount).triggerEvent('Edit a returned order');
         await I.retry(retryCount).waitForText('Select the order');
         const option = await I.grabTextFrom(this.fields.draftOrdersDynamicListOption);
         await I.selectOption(this.fields.draftOrdersDynamicList,option);
         await I.retry(retryCount).click('Continue');
-        await I.wait('90');//2');
-        
+        await I.wait('2');
         await I.retry(retryCount).waitForText('Instructions from the judge');
-        //'Reject solicitor draft order'
+        await I.retry(retryCount).waitForText('Reject solicitor draft order');
         await I.retry(retryCount).click('Continue');
-        await I.wait('90');//2');
+        await I.wait('2');
 }
 };
 
