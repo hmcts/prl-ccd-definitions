a03e9fb918071c7b9178bf16cfb29bddb017603d
Jahnavi <jahnavi1satya@gmail.com>
2024-02-23 10:49:28 +0000

    Add new test EditAReturnedOrder_test.js

---
+++ test/end-to-end/tests/EditAReturnedOrder_test.js
@@ -0,0 +1,21 @@
+const testConfig = require('../config');
+
+/* eslint init-declarations: ["error", "never"]*/
+let caseId;
+
+Feature('Solicitor editing returned Blank order FL404B  by Judge - TS Solicitor application');
+Scenario('Solicitor editing returned Blank order FL404B by Judge @nightly', async({ I }) => {
+  await I.loginAsSolicitor();
+  await I.createNewCaseFL401_TS();
+  await I.statementOfTruthAndSubmit();
+  await I.solicitorDraftAnOrderBlankOrderFL404B();
+  caseId = await I.saveTheCaseIdAndSignout();
+  await I.loginAsJudge();
+  await I.searchForCasesWithId(caseId);
+  //await I.editAnDraftOrderCreatedBySolicitor();
+  await I.selectOrderForReview();
+  caseId = await I.saveTheCaseIdAndSignout();
+  await I.loginAsSolicitor();
+  await I.searchForCasesWithId(caseId);
+  await I.editAReturnedOrderByJudge();
+});
