a62f7f331c57d0d8cfb7440e4bd9c391c0a58db6
Jahnavi <jahnavi1satya@gmail.com>
2024-02-27 13:48:42 +0000

    Update ReturnOrderByJudge.js

--- test/end-to-end/pages/MOScreens/ReturnOrderByJudge.js
+++ test/end-to-end/pages/MOScreens/ReturnOrderByJudge.js
@@ -5,16 +5,16 @@ const manageOrder = require('./ManageOrders');
 module.exports = {
 
   fields: {
-    draftOrdersDynamicList: '#draftOrdersDynamicList',
-    draftOrdersDynamicListOption: '//select[@id="draftOrdersDynamicList"]/option[2]',
+    rejectOrdersDynamicList: '#draftOrdersDynamicList',
+    rejectOrdersDynamicListOption: '//select[@id="draftOrdersDynamicList"]/option[2]',
     AddHearingOutcome: '#fl404CustomFields_fl404bHearingOutcome'
   },
 
   async editAReturnedOrderByJudge() {
     await I.retry(retryCount).triggerEvent('Edit a returned order');
     await I.retry(retryCount).waitForText('Select the order');
-    const option = await I.grabTextFrom(this.fields.draftOrdersDynamicListOption);
-    await I.selectOption(this.fields.draftOrdersDynamicList, option);
+    const option = await I.grabTextFrom(this.fields.rejectOrdersDynamicListOption);
+    await I.selectOption(this.fields.rejectOrdersDynamicList, option);
     await I.retry(retryCount).click('Continue');
     await I.wait('2');
     await I.retry(retryCount).waitForText('Instructions from the judge');
