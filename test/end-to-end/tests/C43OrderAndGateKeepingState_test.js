f144264c3d2bd201c340b4b9bd7296098ba84781
Bala <bala@Balas-MacBook-Pro.local>
2024-02-27 14:15:14 +0000

    update master tag

--- test/end-to-end/tests/C43OrderAndGateKeepingState_test.js
+++ test/end-to-end/tests/C43OrderAndGateKeepingState_test.js
@@ -5,7 +5,7 @@ let caseId;
 
 Feature('Solicitor - Manage order, Gatekeeping & Service of application tests');
 
-Scenario('Draft an Solicitor Order & Move case to Gatekeeping State @nightly', async({ I }) => {
+Scenario('Draft an Solicitor Order & Move case to Gatekeeping State @master', async({ I }) => {
   await I.loginAsSolicitor();
   await I.createSolicitorDummyCase();
   await I.payAndSubmitDummySolicitorCase();
