8b57975ef3670e4872ff3c425090511381a927d8
elizereyes123 <107991381+elizereyes123@users.noreply.github.com>
2024-04-15 16:44:10 +0100

    enable ft tes - C43OrderAndGateKeepingState

--- test/end-to-end/tests/C43OrderAndGateKeepingState_test.js
+++ test/end-to-end/tests/C43OrderAndGateKeepingState_test.js
@@ -18,38 +18,38 @@ Scenario('Draft an Solicitor Order & Move case to Gatekeeping State @regression-
   await I.moveCaseToGateKeeping();
 }).retry(testConfig.TestRetryScenarios);
 
-Scenario('As a Judge edit & change a draft order @regression-suite', async({ I }) => {
+Scenario('As a Judge edit & change a draft order @regression-suite @ptl-regression', async({ I }) => {
   await I.loginAsJudge();
   await I.searchForCasesWithId(caseId);
   await I.editAnDraftOrderCreatedBySolicitor();
 }).retry(testConfig.TestRetryScenarios);
 
-Scenario('As a court admin edit & serve an order @regression-suite', async({ I }) => {
+Scenario('As a court admin edit & serve an order @regression-suite @ptl-regression', async({ I }) => {
   await I.loginAsSwanseaCourtAdmin();
   await I.searchForCasesWithId(caseId);
   await I.serveAnOrder();
   await I.performServiceOfApplication();
 }).retry(testConfig.TestRetryScenarios);
 
-Scenario('As a Case Manager Confidentiality Check @regression-suite', async({ I }) => {
+Scenario('As a Case Manager Confidentiality Check @regression-suite @ptl-regression', async({ I }) => {
   await I.loginAsCaseManager();
   await I.searchForCasesWithId(caseId);
   await I.caseManagerConfidentialityCheck();
 }).retry(testConfig.TestRetryScenarios);
 
-Scenario('As a court admin Verify SOA and Task tab after Confidentiality check @regression-suite', async({ I }) => {
+Scenario('As a court admin Verify SOA and Task tab after Confidentiality check @regression-suite @ptl-regression', async({ I }) => {
   await I.loginAsSwanseaCourtAdmin();
   await I.searchForCasesWithId(caseId);
   await I.verifyPostConfidentialityCheck_Yes();
 }).retry(testConfig.TestRetryScenarios);
 
-Scenario('As an Applicant Solicitor execute Statement of service @regression-suite', async({ I }) => {
+Scenario('As an Applicant Solicitor execute Statement of service @regression-suite @ptl-regression', async({ I }) => {
   await I.loginAsSolicitor();
   await I.searchForCasesWithId(caseId);
   await I.completeStatementOfService();
 }).retry(testConfig.TestRetryScenarios);
 
-Scenario('As a court admin Verify Statement of service in SOA tab @regression-suite', async({ I }) => {
+Scenario('As a court admin Verify Statement of service in SOA tab @regression-suite @ptl-regression', async({ I }) => {
   await I.loginAsSwanseaCourtAdmin();
   await I.searchForCasesWithId(caseId);
   await I.verifyPostStatementOfService();
