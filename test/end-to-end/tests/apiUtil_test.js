e572e2d20a2e05581cc46e266dbd395a8c1a40e9
sreekanthpuligadda <52752138+sreekanthpuligadda@users.noreply.github.com>
2024-06-19 12:55:14 +0100

    api util test updates

--- test/end-to-end/tests/apiUtil_test.js
+++ test/end-to-end/tests/apiUtil_test.js
@@ -9,6 +9,38 @@ const restApiData = JSON.parse(JSON.stringify(restApiDataTemplate));
 /* eslint init-declarations: ["error", "never"]*/
 let caseId;
 
+
+const childAndApplicantProcess = (eventRestObj, startDEventRes) => {
+  const eventDataField = startDEventRes.case_fields.find(field => {
+    return field.id === 'buffChildAndApplicantRelations';
+  });
+
+  eventDataField.value[0].value.childAndApplicantRelation = 'mother';
+  eventDataField.value[0].value.childLivesWith = 'No';
+  eventRestObj.data.buffChildAndApplicantRelations = eventDataField.value;
+};
+
+const childAndRespondentProcess = (eventRestObj, startDEventRes) => {
+  const eventDataField = startDEventRes.case_fields.find(field => {
+    return field.id === 'buffChildAndRespondentRelations';
+  });
+
+  eventDataField.value[0].value.childAndRespondentRelation = 'mother';
+  eventDataField.value[0].value.childLivesWith = 'No';
+  eventRestObj.data.buffChildAndRespondentRelations = eventDataField.value;
+};
+
+const childAndOtherPeopleProcess = (eventRestObj, startDEventRes) => {
+  const eventDataField = startDEventRes.case_fields.find(field => {
+    return field.id === 'buffChildAndOtherPeopleRelations';
+  });
+
+  eventDataField.value[0].value.childAndOtherPeopleRelation = 'mother';
+  eventDataField.value[0].value.childLivesWith = 'No';
+  eventRestObj.data.buffChildAndOtherPeopleRelations = eventDataField.value;
+};
+
+
 Feature('API Util to create case and events ');
 
 
@@ -30,28 +62,39 @@ Scenario('Create case and evenets till submit and pay @debug', async ({ I }) => 
     'Respondent details',
     'Other people in the case',
     'Other children not in the case',
-    // 'Children and applicants',
-    // 'Children and respondents',
-    // 'Children and other people',
-    // 'Allegations of harm',
-    // 'MIAM',
-    // 'Other proceedings',
-    // 'Attending the hearing',
-    // 'International element',
-    // 'Litigation capacity',
-    // 'Welsh language requirements',
+    'Children and applicants',
+    'Children and respondents',
+    'Children and other people',
+    'Allegations of harm',
+    'MIAM',
+    'Other proceedings',
+    'Attending the hearing',
+    'International element',
+    'Litigation capacity',
+    'Welsh language requirements',
     // 'Submit and pay'
 
   ];
+
   for (const event of events) {
     console.log(`************** RUNNING EVENT ${event}`);
     const eventRest = restApiData[event];
-    const res = await eventApi.submitEvent(caseId, eventRest.eventId, eventRest.data);
+
+    let midEventProcess;
+    if (event === 'Children and applicants') {
+      midEventProcess = childAndApplicantProcess;
+    } else if (event === 'Children and respondents') {
+      midEventProcess = childAndRespondentProcess;
+    } else if (event === 'Children and other people') {
+      midEventProcess = childAndOtherPeopleProcess;
+    }
+
+    const res = await eventApi.submitEvent(caseId, eventRest, midEventProcess);
     eventRest.res = res;
   }
 
   console.log(caseId);
-  fs.writeFileSync(`${__dirname}/apoCaseCreationResult.json`, JSON.stringify(restApiData, null, 2));
+  fs.writeFileSync(`${__dirname}/apoCaseCreationResult.json`, JSON.stringify(restApiData, null, '2'));
   // const addCaseNoteRest = restApiData['Add case note'];
   // await eventApi.submitEvent(caseId, addCaseNoteRest.eventId, addCaseNoteRest.data);
 }).retry(testConfig.TestRetryScenarios);
