09907d4ee49e5953f192e5b0e9aaee6dc5f4aeaa
sreekanthpuligadda <52752138+sreekanthpuligadda@users.noreply.github.com>
2024-07-01 17:14:22 +0100

    improved datasetup initated from codecept bootstrap

--- test/end-to-end/tests/apiUtil_test.js
+++ test/end-to-end/tests/apiUtil_test.js
@@ -1,16 +1,19 @@
 /* eslint-disable no-await-in-loop */
 const testConfig = require('../config');
-const DataSetup = require('../restApiData/dataSetup');
-
+const CaseDatasetupManager = require('../restApiData/DataSetupManager');
 
 Feature('API Util to create case and events ');
 
+// eslint-disable-next-line no-unused-vars
+let caseId = null;
+Scenario('Create case and evenets till submit and pay @debug', async({ I }) => {
+  const dataSetupObj = CaseDatasetupManager.getDataSetupForScenario('test1');
 
-Scenario('Create case and evenets till submit and pay', async({ I }) => {
-  await I.wait('1');
-  const dataSetup = new DataSetup();
-  await dataSetup.caseSetupSendToGatekeeper();
-
+  while (dataSetupObj.state === 'running') {
+    await I.wait('2');
+    console.log('waiting for datasetup to complete');
+  }
+  caseId = dataSetupObj.caseId;
   // const addCaseNoteRest = restApiData['Add case note'];
   // await eventApi.submitEvent(caseId, addCaseNoteRest.eventId, addCaseNoteRest.data);
 }).retry(testConfig.TestRetryScenarios);
