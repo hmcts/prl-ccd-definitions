8d29831d6696520716b445a22394fab287e5dd1f
sahaSonali <110189370+sahaSonali@users.noreply.github.com>
2024-08-12 12:40:26 +0100

    Update steps_file.js

--- test/functional/steps_file.js
+++ test/functional/steps_file.js
@@ -15,7 +15,6 @@ module.exports = () => {
       I.fillField('username', config.legalProfessionalUserOne.email);
       I.fillField('password', config.legalProfessionalUserOne.password);
       I.click('Sign in');
-      I.wait('30');
       I.see('Welcome to CCD Admin Web');
     },
     createRole(role) {
