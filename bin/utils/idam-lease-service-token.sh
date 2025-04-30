eafb9856b222c6220808adaae4a09e2c2aa25652
SomeshRA <somesh.acharya1@hmcts.net>
2025-04-30 12:48:49 +0100

    FPVTL-563: Updated iam lease script

--- bin/utils/idam-lease-service-token.sh
+++ bin/utils/idam-lease-service-token.sh
@@ -5,12 +5,12 @@ set -eux
 microservice=${1}
 oneTimePassword=${2}
 
-echo "microservice is ${microservice}"
+echo "microservice is '${microservice}'"
 
 curl --insecure --fail --show-error --silent -X POST \
   ${SERVICE_AUTH_PROVIDER_API_BASE_URL:-http://localhost:4502}/lease \
   -H "Content-Type: application/json" \
   -d '{
     "microservice": "prl-cos-api",
-    "oneTimePassword": "${oneTimePassword}"
+    "oneTimePassword": "'${oneTimePassword}'"
   }'
