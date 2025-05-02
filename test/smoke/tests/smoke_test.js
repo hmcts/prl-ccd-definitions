a80299ca9a9df82b7407b4a16d2d9ed08991d7e8
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2025-05-02 15:38:02 +0100

    PR-build-fix: remove STs 2

--- test/smoke/tests/smoke_test.js
+++ test/smoke/tests/smoke_test.js
@@ -1,9 +1,9 @@
 Feature('Smoke tests @smoke-tests');
 Scenario('Sign in as local authority and create a case', async({ I }) => {
   // await I.loginAsSolicitor();
-  // const caseId = await I.createCaseAndReturnID();
-  // await I.navigateToCaseList();
-  // console.log(`case id is ${caseId}`);
+  const caseId = await I.createCaseAndReturnID();
+  await I.navigateToCaseList();
+  console.log(`case id is ${caseId}`);
   // await I.searchForCasesWithId(caseId); --This check is ignored due to a bug FPET-903
   // await I.seeCaseInSearchResult(caseId); --This check is ignored due to a bug FPET-903
 }).retry(1);
