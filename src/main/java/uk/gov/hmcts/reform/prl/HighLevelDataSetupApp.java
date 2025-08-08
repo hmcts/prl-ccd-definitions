37fca396f819cd9936e122ba79323211c99a0766
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2025-08-08 09:31:28 +0100

    FPVTL-876: updated xls2

--- src/main/java/uk/gov/hmcts/reform/prl/HighLevelDataSetupApp.java
+++ src/main/java/uk/gov/hmcts/reform/prl/HighLevelDataSetupApp.java
@@ -63,7 +63,7 @@ public class HighLevelDataSetupApp extends DataLoaderToDefinitionStore {
 
     @Override
     protected boolean shouldTolerateDataSetupFailure() {
-        return true;
+        return false;
     }
 
     @Override
