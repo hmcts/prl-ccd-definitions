478a876887769aa48e6e8f673bdb8876588f0b99
SomeshRA <somesh.acharya1@hmcts.net>
2026-01-19 14:49:22 +0000

    FPVLT-1864: Added hearing system user

--- bin/utils/add-system-user-case-allocator-role.sh
+++ bin/utils/add-system-user-case-allocator-role.sh
@@ -61,4 +61,70 @@ curl --silent --show-error --location "${ROLE_ASSIGNMENT_URL}/am/role-assignment
             }
         ]
     }
+
+curl --silent --show-error --location "${ROLE_ASSIGNMENT_URL}/am/role-assignments" \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Bearer ${ACCESS_TOKEN}" \
+  --header "ServiceAuthorization: Bearer ${SERVICE_TOKEN}" \
+  --data @- <<EOF
+    {
+        "roleRequest": {
+            "id": "6455c84c-e77d-4c4f-9759-bf4a93a8e971",
+            "clientId": "fis_hmc_api",
+            "authenticatedUserId": null,
+            "correlationId": null,
+            "assignerId": "6455c84c-e77d-4c4f-9759-bf4a93a8e971",
+            "requestType": null,
+            "process": "private-law-system-users",
+            "reference": "private-law-hearings-system-user",
+            "replaceExisting": true,
+            "roleAssignmentId": null,
+            "status": null,
+            "created": null,
+            "log": null,
+            "byPassOrgDroolRule": false
+        },
+        "requestedRoles": [
+            {
+                "id": null,
+                "actorIdType": "IDAM",
+                "actorId": "6455c84c-e77d-4c4f-9759-bf4a93a8e971",
+                "roleType": "ORGANISATION",
+                "roleName": "hearing-viewer",
+                "classification": "PUBLIC",
+                "grantType": "STANDARD",
+                "roleCategory": "SYSTEM",
+                "readOnly": false,
+                "beginTime": "2026-01-19T13:58:00.946954Z",
+                "endTime": null,
+                "created": null,
+                "authorisations": null,
+                "attributes": {
+                  "caseType": "PRLAPPS",
+                  "substantive": "N",
+                  "jurisdiction": "PRIVATELAW"
+              },
+              {
+                "id": null,
+                "actorIdType": "IDAM",
+                "actorId": "6455c84c-e77d-4c4f-9759-bf4a93a8e971",
+                "roleType": "ORGANISATION",
+                "roleName": "hearing-manager",
+                "classification": "PUBLIC",
+                "grantType": "STANDARD",
+                "roleCategory": "SYSTEM",
+                "readOnly": false,
+                "beginTime": "2026-01-19T13:58:00.946954Z",
+                "endTime": null,
+                "created": null,
+                "authorisations": null,
+                "attributes": {
+                  "caseType": "PRLAPPS",
+                  "substantive": "N",
+                  "jurisdiction": "PRIVATELAW"
+                }
+            },
+        ]
+    }
 EOF
