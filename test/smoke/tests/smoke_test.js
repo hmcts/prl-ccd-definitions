f366a075d93b9ac95b54c42b04d1c3ae440a04fc
SomeshRA <somesh.acharya1@hmcts.net>
2026-03-25 18:21:51 +0000

    FPVTL-2502: dummy test

--- test/smoke/tests/smoke_test.js
+++ test/smoke/tests/smoke_test.js
@@ -1,5 +1,8 @@
 Feature('Smoke tests @smoke-tests');
 Scenario('dummy test does nothing', ({ I }) => {
+  I.say = function (runningDummyTest) {
+    
+  };
   // minimal no-op assertion
   I.say('Running dummy test');
 });
