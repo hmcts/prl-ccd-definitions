469a016f1fac816b56642376ffbf5ff4466d1dd2
sunilrochani <sunil.rochani@hotmail.com>
2026-03-25 17:54:43 +0000

    FPVTL-2502: Shutter Manage Case

--- test/smoke/tests/smoke_test.js
+++ test/smoke/tests/smoke_test.js
@@ -1,9 +1,4 @@
 Feature('Smoke tests @smoke-tests');
 Scenario('Sign in as Solicitor and create a case', async({ I }) => {
   await I.loginAsSolicitor();
-  const caseId = await I.createCaseAndReturnID();
-  await I.navigateToCaseList();
-  console.log('case id is ${caseId}');
-  await I.searchForCasesWithId(caseId);
-  await I.seeCaseInSearchResult(caseId);
 }).retry(1);
