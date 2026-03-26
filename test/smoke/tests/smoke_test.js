3f6b645739fb1e4ee789a443df41d20dc4ee5b07
SomeshRA <somesh.acharya1@hmcts.net>
2026-03-26 19:25:46 +0000

    FPVTL-2523: Retries added

--- test/smoke/tests/smoke_test.js
+++ test/smoke/tests/smoke_test.js
@@ -1,7 +1,8 @@
 const assert = require('assert');
 
-const INITIAL_WAIT = 2;
-
+const INITIAL_WAIT = 5;
+const RETRIES = 3;
+const MIN_TIMEOUT = 1000;
 Feature('Smoke tests @smoke-tests');
 
 Scenario('Sign in as Solicitor and create a case', async({ I }) => {
@@ -11,14 +12,15 @@ Scenario('Sign in as Solicitor and create a case', async({ I }) => {
   console.log(`case id is ${caseId}`);
   assert.ok(caseId, 'Case ID should be defined');
 
-  await I.wait(INITIAL_WAIT);
+
 
   const pageUrl = `${process.env.XUI_WEB_URL}`.concat('/case-details/PRIVATELAW/PRLAPPS/').concat(caseId);
 
   console.log(`Page url is ${pageUrl}`);
   await I.amOnPage(pageUrl);
+  await I.wait(INITIAL_WAIT);
   const formattedCaseId = caseId.replace(/(\d{4})(\d{4})(\d{4})(\d{4})/, '$1-$2-$3-$4');
   console.log(`Formatted case id: ${formattedCaseId}`);
   // Verify case ID visible on page
-  await I.see(formattedCaseId);
+  await I.retry({ retries: RETRIES, minTimeout: MIN_TIMEOUT }).see(formattedCaseId);
 }).retry(1);
