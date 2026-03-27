44ff83222b39d868a47f68db2d01499ffbd96303
SomeshRA <somesh.acharya1@hmcts.net>
2026-03-27 11:49:13 +0000

    FPVTL-2523: Added Case page js

--- test/smoke/pages/CasePage.js
+++ test/smoke/pages/CasePage.js
@@ -1,6 +1,8 @@
 const { I } = inject();
 const config = require('../config');
 
+const INITIAL_WAIT = 15;
+
 module.exports = {
   async loadCase(caseId) {
     const pageUrl = `${config.baseUrl}`
@@ -11,5 +13,11 @@ module.exports = {
       .concat(config.definition.caseType)
       .concat(caseId);
     await I.amOnPage(pageUrl);
+  },
+
+  async searchForCaseId(caseId) {
+    const formattedCaseId = caseId.replace(/(\d{4})(\d{4})(\d{4})(\d{4})/, '$1-$2-$3-$4');
+    await I.waitForText(formattedCaseId, INITIAL_WAIT);
+    await I.see(formattedCaseId);
   }
 };
--- test/smoke/tests/smoke_test.js
+++ test/smoke/tests/smoke_test.js
@@ -1,8 +1,5 @@
 const assert = require('assert');
 
-const INITIAL_WAIT = 15;
-const RETRIES = 3;
-const MIN_TIMEOUT = 1000;
 Feature('Smoke tests @smoke-tests');
 
 Scenario('Sign in as Solicitor and create a case', async({ I }) => {
@@ -13,10 +10,5 @@ Scenario('Sign in as Solicitor and create a case', async({ I }) => {
   assert.ok(caseId, 'Case ID should be defined');
 
   await I.loadCase(caseId);
-
-  const formattedCaseId = caseId.replace(/(\d{4})(\d{4})(\d{4})(\d{4})/, '$1-$2-$3-$4');
-  console.log(`Formatted case id: ${formattedCaseId}`);
-  await I.waitForText(formattedCaseId, INITIAL_WAIT);
-  // Verify case ID visible on page
-  await I.retry({ retries: RETRIES, minTimeout: MIN_TIMEOUT }).see(formattedCaseId);
+  await I.searchForCaseId(caseId);
 }).retry(1);
